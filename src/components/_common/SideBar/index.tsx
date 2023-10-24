1f8b7dc073423c37cb8fb173e7483bae455c72b2
JiYoon <angella990825@gmail.com>
2023-10-24 22:54:56 +0900

    ✨ Feat: #2 - 공통 컴포넌트 SideBar 추가

---
+++ src/components/_common/SideBar/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { useState } from "react";
+import Link from "next/link";
+import { Box, Theme } from "@radix-ui/themes";
+
+interface SideBarItem {
+  id?: string;
+  label?: string;
+  href: string;
+}
+
+interface SideBarProps {
+  sidebarItems: SideBarItem[];
+  children?: ReactNode;
+  boxStyles: string;
+  itemStyles: string;
+}
+
+const SideBar = ({
+  children,
+  sidebarItems,
+  itemStyles,
+  boxStyles,
+}: SideBarProps) => {
+  const [selectedItem, setSelectedItem] = useState(0);
+
+  return (
+    <Theme>
+      <Box
+        className={`${boxStyles} border-[#BDE3FF] flex flex-col items-center border-solid border-5 rounded-[20px]`}
+      >
+        {sidebarItems.map((item, id) => (
+          <Link
+            href={item.href}
+            key={id}
+          >
+            <div
+              className={`${itemStyles} text-[18px] font-bold transition duration-100 ${
+                selectedItem === id
+                  ? "bg-[#BDE3FF] text-[#2F69FF]"
+                  : " hover:bg-[#b4b4b4]"
+              }`}
+              onClick={() => setSelectedItem(id)}
+            >
+              {item.label}
+            </div>
+          </Link>
+        ))}
+        {children}
+      </Box>
+    </Theme>
+  );
+};
+
+export default SideBar;
