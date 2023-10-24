ee09c2ddf381486b46586101e2c8e43cfbe46259
Cheshier <chungup5495@gmail.com>
2023-10-25 00:34:43 +0900

    ✨ Feat: Avatar 공통 컴포넌트 개발

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   eslint: {
     dirs: ["src"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
