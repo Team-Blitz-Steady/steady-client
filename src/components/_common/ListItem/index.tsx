93926c52188ce58c43fad84a36bfe7252d54c532
JiYoon <angella990825@gmail.com>
2023-10-25 23:06:46 +0900

    ✨ Feat: #3 - 공통 컴포넌트 ListItem 추가

---
+++ src/components/_common/ListItem/index.tsx
@@ -0,0 +1,53 @@
+import type { ReactNode } from "react";
+import Icon from "@/components/Icon";
+import clsx from "clsx";
+import type { SteadyInfoType } from "@/app/page";
+
+interface ListItemProps {
+  children?: ReactNode;
+  object: SteadyInfoType[];
+  className?: string;
+  listType: "steady" | "form";
+}
+
+const ListItem = ({ className, object, listType }: ListItemProps) => {
+  return (
+    <div>
+      {object.map((item, id) => (
+        <div
+          key={id}
+          className={clsx(
+            "border-black flex h-140 cursor-pointer items-center justify-between border-2 border-solid px-40 py-50",
+            className,
+          )}
+        >
+          <div className="text-black text-26 font-bold">{item.title}</div>
+          <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-30">
+              <div className="text-bold text-15 text-st-gray-100">
+                생성일 {item.createdAt}
+              </div>
+              {item.isAdmin && (
+                <>
+                  {listType === "steady" ? (
+                    <>
+                      <Icon label={"config"} />
+                      <Icon label={"trash"} />
+                    </>
+                  ) : (
+                    <>
+                      <Icon label={"edit"} />
+                      <Icon label={"trash"} />
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListItem;
