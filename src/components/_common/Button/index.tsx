0409cdeb48d82e2ff0f52bd7624a4815bf66e361
JiYoon <angella990825@gmail.com>
2023-10-25 16:49:11 +0900

    ✨ Feat: #6 - Button 공통 style은 buttonsConfig에서 제외

--- src/components/_common/Button/index.tsx
+++ src/components/_common/Button/index.tsx
@@ -1,22 +1,22 @@
 import type { ReactNode } from "react";
 
 export const buttonsConfig = {
-  create: "w-[165px] h-[50px] rounded-[6px] bg-[#9dd5ff] shadow-md",
-  lg: "w-[200px] h-[80px] rounded-[6px] bg-[#ff5353]",
+  create: "w-[165px] h-[50px]  bg-[#9dd5ff]",
+  lg: "w-[200px] h-[80px] bg-[#ff5353]",
   sm: {
-    white: "w-[115px] h-[40px] rounded-[15px] bg-[#ffffff] shadow-md",
-    blue: "w-[115px] h-[40px] rounded-[15px] bg-[#0d99ff] shadow-md",
-    red: "w-[115px] h-[40px] rounded-[15px] bg-[#ff5353] shadow-md",
-    green: "w-[115px] h-[40px] rounded-[15px] bg-[#35cc00] shadow-md",
+    white: "w-[115px] h-[40px] bg-[#ffffff]",
+    blue: "w-[115px] h-[40px] bg-[#0d99ff]",
+    red: "w-[115px] h-[40px] bg-[#ff5353]",
+    green: "w-[115px] h-[40px] bg-[#35cc00]",
   },
-  sm2: "w-[130px] h-[40px] rounded-[15px] bg-[#0d99ff] shadow-md",
-  sm3: "w-[160px] h-[40px] rounded-[15px] bg-[#0d99ff] shadow-md",
-  md: "w-[130px] h-[50px] rounded-[15px] bg-[#ff5353] shadow-md",
+  sm2: "w-[130px] h-[40px] bg-[#0d99ff]",
+  sm3: "w-[160px] h-[40px] bg-[#0d99ff]",
+  md: "w-[130px] h-[50px] bg-[#ff5353]",
   modal: {
-    white: "w-[130px] h-[55px] rounded-[15px] bg-[#ffffff] shadow-md",
-    blue: "w-[130px] h-[55px] rounded-[15px] bg-[#0d99ff] shadow-md",
-    red: "w-[130px] h-[55px] rounded-[15px] bg-[#ff5353] shadow-md",
-    green: "w-[130px] h-[55px] rounded-[15px] bg-[#35cc00] shadow-md",
+    white: "w-[130px] h-[55px] bg-[#ffffff]",
+    blue: "w-[130px] h-[55px] bg-[#0d99ff]",
+    red: "w-[130px] h-[55px] bg-[#ff5353]",
+    green: "w-[130px] h-[55px] bg-[#35cc00]",
   },
 };
 
@@ -26,7 +26,11 @@ interface buttonProps {
 }
 
 const Button = ({ children, buttonStyles }: buttonProps) => {
-  return <button className={`${buttonStyles}`}>{children}</button>;
+  return (
+    <button className={`${buttonStyles} rounded-[15px] shadow-md`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
