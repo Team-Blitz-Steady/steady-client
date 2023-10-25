582541473fcb31a72cb3893562a8ed3a1222904a
Cheshier <chungup5495@gmail.com>
2023-10-25 16:45:45 +0900

    💄 Style: tailwind.config에 색상 정보 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    colors: {
+      "st-primary": "#5585ff",
+      "st-red": "#ff5353",
+      "st-green": "#35cc00",
+      "st-gray-100": "#b4b4b4",
+      "st-gray-200": "#8a8a8a",
+      "st-gray-250": "#858585",
+      "st-gray-400": "#6c6c6c",
+    },
     extend: {},
   },
   plugins: [],
