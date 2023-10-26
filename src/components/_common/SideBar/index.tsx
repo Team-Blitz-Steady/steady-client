230a363985a7dd0870e47fe757d5fc010f8c653a
JiYoon <angella990825@gmail.com>
2023-10-26 17:04:54 +0900

    🚨 Fix: #2 - id, label 옵셔널 제거, Theme 제거

--- src/components/_common/SideBar/index.tsx
+++ src/components/_common/SideBar/index.tsx
@@ -3,11 +3,11 @@
 import type { PropsWithChildren } from "react";
 import { useState } from "react";
 import Link from "next/link";
-import { Box, Theme } from "@radix-ui/themes";
+import { Box } from "@radix-ui/themes";
 
 interface SideBarItem {
-  id?: string;
-  label?: string;
+  id: string;
+  label: string;
   href: string;
 }
 
@@ -26,30 +26,28 @@ const SideBar = ({
   const [selectedItem, setSelectedItem] = useState(0);
 
   return (
-    <Theme>
-      <Box
-        className={`${boxStyles} flex flex-col items-center rounded-20 border-5 border-solid border-st-gray-100`}
-      >
-        {sidebarItems.map((item, id) => (
-          <Link
-            href={item.href}
-            key={id}
+    <Box
+      className={`${boxStyles} flex flex-col items-center rounded-20 border-5 border-solid border-st-gray-100`}
+    >
+      {sidebarItems.map((item, id) => (
+        <Link
+          href={item.href}
+          key={id}
+        >
+          <div
+            className={`${itemStyles} text-18 font-bold transition duration-100 ${
+              selectedItem === id
+                ? "bg-st-skyblue text-st-primary"
+                : " hover:bg-st-gray-50"
+            }`}
+            onClick={() => setSelectedItem(id)}
           >
-            <div
-              className={`${itemStyles} text-18 font-bold transition duration-100 ${
-                selectedItem === id
-                  ? "bg-st-skyblue text-st-primary"
-                  : " hover:bg-st-gray-50"
-              }`}
-              onClick={() => setSelectedItem(id)}
-            >
-              {item.label}
-            </div>
-          </Link>
-        ))}
-        {children}
-      </Box>
-    </Theme>
+            {item.label}
+          </div>
+        </Link>
+      ))}
+      {children}
+    </Box>
   );
 };
 
