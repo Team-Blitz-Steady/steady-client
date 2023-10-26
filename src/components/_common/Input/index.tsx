0a95845b80aae1532455c867e199dd86d30c485f
Luis <ljw927@naver.com>
2023-10-26 14:14:32 +0900

    📝 Chore: #5 - px 제거

--- src/components/_common/Input/index.tsx
+++ src/components/_common/Input/index.tsx
@@ -7,9 +7,9 @@ const Input = ({ inputName = "" }) => {
   switch (inputName) {
     case "search-input":
       input = (
-        <div className="hidden h-[40px] items-center justify-center rounded-[12px] border border-2 border-solid border-st-gray-100 bg-input-bg p-[5px] outline-none md:flex md:w-1/4 xl:w-[350px]">
+        <div className="hidden h-40 items-center justify-center rounded-12 border border-2 border-solid border-st-gray-100 bg-input-bg p-5 outline-none md:flex md:w-1/4 xl:w-350">
           <input
-            className="h-[35px] w-full rounded-[12px] bg-input-bg p-[5px] font-bold outline-none"
+            className="h-35 w-full rounded-12 bg-input-bg p-5 font-bold outline-none"
             type="text"
             placeholder="검색어를 입력해주세요."
           />
@@ -27,7 +27,7 @@ const Input = ({ inputName = "" }) => {
       input = (
         <div>
           <input
-            className="h-[56px] w-[523px] items-center rounded-[12px] border border-2 border-solid border-st-gray-100 pl-[5px] pr-[5px] text-center text-lg outline-none"
+            className="h-56 w-523 items-center rounded-12 border border-2 border-solid border-st-gray-100 pl-5 pr-5 text-center text-lg outline-none"
             type="text"
             placeholder="닉네임을 입력해주세요."
           />
@@ -38,7 +38,7 @@ const Input = ({ inputName = "" }) => {
       input = (
         <div>
           <input
-            className="h-[60px] w-[1000px] pl-[5px] pr-[5px] text-3xl font-bold outline-none"
+            className="h-60 w-1000 pl-5 pr-5 text-3xl font-bold outline-none"
             type="text"
             placeholder="스테디명"
           />
@@ -49,7 +49,7 @@ const Input = ({ inputName = "" }) => {
       input = (
         <div>
           <input
-            className="h-[60px] w-[1000px] pl-[5px] pr-[5px] text-3xl font-bold outline-none"
+            className="h-60 w-1000 pl-5 pr-5 text-3xl font-bold outline-none"
             type="text"
             placeholder="제목을 입력해주세요."
           />
@@ -60,7 +60,7 @@ const Input = ({ inputName = "" }) => {
       input = (
         <div>
           <input
-            className="h-[25px] w-[250px] pl-[5px] pr-[5px] text-lg outline-none"
+            className="h-25 w-250 pl-5 pr-5 text-lg outline-none"
             type="text"
             placeholder="태그를 설정하세요 (최대 5개)"
           />
@@ -69,9 +69,9 @@ const Input = ({ inputName = "" }) => {
       break;
     case "introduce-input":
       input = (
-        <div className="flex h-[64px] items-center justify-center rounded-[12px] border border-2 border-solid border-st-gray-100 p-[10px] outline-none md:w-1/2 xl:w-[716px]">
+        <div className="flex h-64 items-center justify-center rounded-12 border border-2 border-solid border-st-gray-100 p-10 outline-none md:w-1/2 xl:w-716">
           <input
-            className="h-[60px] w-full rounded-[12px] p-[5px] text-xl font-bold outline-none"
+            className="h-60 w-full rounded-12 p-5 text-xl font-bold outline-none"
             type="text"
             placeholder="한 줄 소개로 자신을 표현해 주세요!"
           />
