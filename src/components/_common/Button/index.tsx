8a98174f304edba66468f7c3606968746aa78da2
JiYoon <angella990825@gmail.com>
2023-10-26 16:29:48 +0900

    🔨 Refactor: #6 - Button 컴포넌트 size 객체 생성

--- src/components/_common/Button/index.tsx
+++ src/components/_common/Button/index.tsx
@@ -1,24 +1,11 @@
 import type { PropsWithChildren } from "react";
 import { clsx } from "clsx";
 
-export const buttonsConfig = {
-  create: "w-165 h-50 bg-st-primary",
-  lg: "w-200 h-80 bg-st-red",
-  sm: {
-    white: "w-115 h-40 bg-white",
-    blue: "w-115 h-40 bg-st-primary",
-    red: "w-115 h-40 bg-st-red",
-    green: "w-115 h-40 bg-st-green",
-  },
-  sm2: "w-130 h-40 bg-st-primary",
-  sm3: "w-160 h-40 bg-st-primary",
-  md: "w-130 h-50 bg-st-red",
-  modal: {
-    white: "w-130 h-55 bg-white",
-    blue: "w-130 h-55 bg-st-primary",
-    red: "w-130 h-55 bg-st-red",
-    green: "w-130 h-55 bg-st-green",
-  },
+export const buttonSize = {
+  xl: "w-165 h-50 rounded-6",
+  lg: "w-140 h-50",
+  md: "w-115 h-50",
+  sm: "w-90 h-35",
 };
 
 const Button = ({
