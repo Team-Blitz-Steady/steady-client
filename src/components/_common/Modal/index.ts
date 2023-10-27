e181d23cae9d8ea5187980a378fbaab677ed5084
JiYoon <angella990825@gmail.com>
2023-10-27 14:36:10 +0900

    ✨ Feat(#4) - 모달 index 파일 추가

---
+++ src/components/_common/Modal/index.ts
@@ -0,0 +1,4 @@
+export { default as AlertModal } from "./AlertModal";
+export { default as LoginModal } from "./LoginModal";
+export { default as UserModal } from "./UserModal";
+export { default as InfoModal } from "./InfoModal";
