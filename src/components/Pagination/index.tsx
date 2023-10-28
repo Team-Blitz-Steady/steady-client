62d0108c9bfe5de5b544dc474d61904eeae7da00
Musix <ljw927@naver.com>
2023-10-28 23:45:37 +0900

    📝 Chore(#43): lint 문제 해결

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,34 @@
+import Button, { buttonSize } from "../_common/Button";
+
+interface PaginationProps {
+  totalPosts: number;
+  limit: number;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Pagination = ({ totalPosts, limit, page, setPage }: PaginationProps) => {
+  const numPages = Math.ceil(totalPosts / limit) + page;
+
+  return (
+    <div>
+      <div>
+        {Array(numPages)
+          .fill(1)
+          .map((_, i) => {
+            return (
+              <Button
+                key={i + 1}
+                className={`${buttonSize.sm} w-35 hover:bg-st-primary hover:text-st-white`}
+                onClick={() => setPage(i + 1)}
+              >
+                {i + 1}
+              </Button>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
