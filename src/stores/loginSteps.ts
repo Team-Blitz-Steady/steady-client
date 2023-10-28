dadb84f343d99fa01d8be5c2911fdb285069b6d4
JiYoon <angella990825@gmail.com>
2023-10-28 22:11:12 +0900

    ✨ Feat(#52): 로그인 스텝 상태관리 추가 + 로컬 스토리지에서도 관리

---
+++ src/stores/loginSteps.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const StorageKey = "loginSteps-key";
+
+interface LoginStepsState {
+  steps: number;
+  setIncreaseSteps: () => void;
+  setDecreaseSteps: () => void;
+}
+
+const useLoginStepsStore = create(
+  persist<LoginStepsState>(
+    (set, get) => ({
+      steps: 1,
+      setIncreaseSteps: () => set({ steps: get().steps + 1 }),
+      setDecreaseSteps: () => set({ steps: get().steps - 1 }),
+    }),
+    {
+      name: StorageKey,
+    },
+  ),
+);
+
+export default useLoginStepsStore;
