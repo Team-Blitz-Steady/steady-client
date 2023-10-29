880adc35965286f004d29f7af8ec5bd1a838eda5
sscoderati <chungup5495@gmail.com>
2023-10-29 15:23:32 +0900

    🚨 Fix(#55): Avatar 컴포넌트 에러 수정 외 1건
    - 가로 세로 비율이 동일하지 않은 이미지 src 추가 시 rounded-full 스타일이 적용되지 않았던 문제 수정
    - alt를 prop으로 받을 수 있도록 변경
    - 현재 alt 값을 Fallback UI로 보여주는 중

--- src/components/_common/Avatar/index.tsx
+++ src/components/_common/Avatar/index.tsx
@@ -1,19 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 interface AvatarProps {
   src: string;
+  alt?: string;
   size: number;
 }
 
-const Avatar = ({ src, size }: AvatarProps) => {
+const Avatar = ({ src, alt = "S", size }: AvatarProps) => {
   return (
-    <Image
-      className={"rounded-full"}
-      src={src}
-      alt={"S"}
-      width={size}
-      height={size}
-    />
+    <AvatarPrimitive.Root
+      className={`inline-flex h-[${size}px] w-[${size}px] select-none items-center justify-center overflow-hidden rounded-full align-middle`}
+    >
+      <AvatarPrimitive.Image
+        className={"h-full w-full rounded-[inherit] object-cover"}
+        src={src}
+        alt={alt}
+      />
+      <AvatarPrimitive.AvatarFallback>{alt}</AvatarPrimitive.AvatarFallback>
+    </AvatarPrimitive.Root>
   );
 };
 
