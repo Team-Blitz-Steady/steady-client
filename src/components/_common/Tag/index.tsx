82c14762171067d1a59054634819476b1ea781da
JiYoon <angella990825@gmail.com>
2023-10-29 21:47:01 +0900

    💄 Style(#51): Tag 반응형 구현

--- src/components/_common/Tag/index.tsx
+++ src/components/_common/Tag/index.tsx
@@ -7,10 +7,10 @@ interface TagProps extends HTMLAttributes<HTMLDivElement> {
 const Tag = ({ status, ...props }: PropsWithChildren<TagProps>) => {
   return (
     <div
-      className="h-40 w-80 rounded-50 bg-st-primary p-5"
+      className="h-40 w-80 rounded-50 bg-st-primary p-5 "
       {...props}
     >
-      <div className="h-full w-full rounded-50 bg-st-white text-center text-20 font-bold">
+      <div className="h-full w-full rounded-50 bg-st-white text-center text-20 font-bold max-mobile:text-15">
         {status === "모집" ? "모집" : "마감"}
       </div>
     </div>
