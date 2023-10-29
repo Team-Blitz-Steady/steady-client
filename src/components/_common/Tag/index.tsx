d8bed099274958244fee818247f2d8c03a99c05b
JiYoon <angella990825@gmail.com>
2023-10-29 16:32:00 +0900

    ✨ Feat(#51): Tag 컴포넌트 추가
    모집/마감을 구분하는 공통 컴포넌트 Tag 추가

---
+++ src/components/_common/Tag/index.tsx
@@ -0,0 +1,20 @@
+import type { HTMLAttributes, PropsWithChildren } from "react";
+
+interface TagProps extends HTMLAttributes<HTMLDivElement> {
+  status: string;
+}
+
+const Tag = ({ status, ...props }: PropsWithChildren<TagProps>) => {
+  return (
+    <div
+      className="h-40 w-80 rounded-50 bg-st-primary p-5"
+      {...props}
+    >
+      <div className="h-full w-full rounded-50 bg-st-white text-center text-20 font-bold">
+        {status === "모집" ? "모집" : "마감"}
+      </div>
+    </div>
+  );
+};
+
+export default Tag;
