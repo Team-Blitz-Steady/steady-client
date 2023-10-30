23eca0053af17519a2273b53069dc58957ddb1cb
JiYoon <angella990825@gmail.com>
2023-10-30 12:48:54 +0900

    ✨ Feat(#52): steps의 상태 변경 해 줄 setSteps 추가

--- src/stores/loginSteps.ts
+++ src/stores/loginSteps.ts
@@ -5,14 +5,18 @@ const StorageKey = "loginSteps-key";
 
 interface LoginStepsState {
   steps: number;
+  // eslint-disable-next-line no-unused-vars
+  setSteps: (steps: number) => void;
   setIncreaseSteps: VoidFunction;
   setDecreaseSteps: VoidFunction;
 }
 
+// TODO: 로그인 연결하면 steps 0으로 수정
 const useLoginStepsStore = create(
   persist<LoginStepsState>(
     (set, get) => ({
       steps: 1,
+      setSteps: (steps) => set({ steps }),
       setIncreaseSteps: () => set({ steps: get().steps + 1 }),
       setDecreaseSteps: () => set({ steps: get().steps - 1 }),
     }),
