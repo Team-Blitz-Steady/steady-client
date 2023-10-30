4ab84760a2cb77a663e0a2ed898d6a0d4db64147
JiYoon <angella990825@gmail.com>
2023-10-30 12:47:22 +0900

    🔨 Refactor(#52): LoginModal import 명 변경

--- src/components/_common/AppBar/index.tsx
+++ src/components/_common/AppBar/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import LogoImage from "@/images/logo.png";
 import { BellIcon } from "@radix-ui/react-icons";
-import LoginModalContainer from "../Modal/LoginModal";
+import LoginModal from "../Modal/LoginModal";
 
 interface AppBarProps {
   isLogin: boolean;
@@ -11,7 +11,7 @@ export const appBarTextStyles = "text-lg font-bold";
 
 const AppBar = ({ isLogin = false }: AppBarProps) => {
   return (
-    <div className="md:w-5/6 xl:w-1120 flex items-center justify-between pb-30 pt-30">
+    <div className="flex items-center justify-between pb-30 pt-30 md:w-5/6 xl:w-1120">
       <Image
         src={LogoImage}
         alt="스테디 로고"
@@ -26,7 +26,7 @@ const AppBar = ({ isLogin = false }: AppBarProps) => {
           <div className={appBarTextStyles}>프로필</div>
         </div>
       ) : (
-        <LoginModalContainer
+        <LoginModal
           trigger={
             <div className={`${appBarTextStyles} cursor-pointer`}>로그인</div>
           }
