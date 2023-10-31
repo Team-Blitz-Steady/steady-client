fc91c02747d15de60b891ec391b11517c54d6872
Musix <ljw927@naver.com>
2023-10-31 16:13:45 +0900

    ✨ Feat(#43): StickyButton 및 Top scroll 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Avatar } from "@radix-ui/themes";
 import Button, { buttonSize } from "@/components/_common/Button";
 import Icon from "@/components/_common/Icon";
 import Input from "@/components/_common/Input";
+import StickyButton from "@/components/_common/StickyButton";
 import CopyRight from "../../public/images/copyright.svg";
 import Dolphin from "../../public/images/dolphin.png";
 import First from "../../public/images/first.svg";
@@ -110,7 +111,7 @@ const Home = () => {
   ];
 
   return (
-    <main className="flex flex-col items-center">
+    <main className="relative flex flex-col items-center">
       <div className="relative flex h-380 w-screen transition">
         <div
           className={`${
@@ -548,6 +549,19 @@ const Home = () => {
           <div className="font-bold">서비스 소개</div>
         </div>
       </footer>
+      <div className="fixed bottom-40 right-10 z-10 flex gap-10">
+        <div
+          className="flex h-65 w-65 cursor-pointer items-center justify-center rounded-full bg-st-primary"
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        >
+          <div className="flex h-55 w-55 items-center justify-center rounded-full bg-st-white">
+            <div className="text-17 font-bold">TOP</div>
+          </div>
+        </div>
+        <StickyButton onClick={() => console.log("hi")} />
+      </div>
     </main>
   );
 };
--- src/components/_common/StickyButton/index.tsx
+++ src/components/_common/StickyButton/index.tsx
@@ -9,7 +9,7 @@ const StickyButton = ({ onClick }: StickyButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="absolute bottom-70 right-10 flex w-160 items-center justify-center rounded-full pb-5 pl-10 pr-2 pt-5 shadow-md mobile:bottom-30"
+      className="mobile:bottom-30 flex w-160 items-center justify-center rounded-full bg-st-white pb-5 pl-10 pr-2 pt-5 shadow-md"
     >
       <div className="text-lg font-bold">문의하기</div>
       <Image
