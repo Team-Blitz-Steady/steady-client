8f862ba5c500599bd786cd871093ba227334ff9c
sscoderati <chungup5495@gmail.com>
2023-10-31 12:28:09 +0900

    ✨ Feat(#20): SingleSelector 추가
    - 목록 중 하나를 선택하는 데 사용
    - 선택된 항목을 Label로 표시

--- src/components/_common/Selector/core/select.tsx
+++ src/components/_common/Selector/core/select.tsx
@@ -49,7 +49,7 @@ const SelectContent = React.forwardRef<
     >
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
+          "p-4",
           position === "popper" &&
             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
         )}
@@ -67,7 +67,7 @@ const SelectLabel = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
     ref={ref}
-    className={cn("py-6 pl-32 pr-8 text-sm font-semibold", className)}
+    className={cn("py-6 pl-32 pr-8 text-sm font-medium", className)}
     {...props}
   />
 ));
@@ -80,7 +80,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-6 pl-32 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-6 pl-32 pr-8 text-sm font-normal outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className,
     )}
     {...props}
