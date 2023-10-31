36ced8b50394eb6ce135a884f4c67bff53bfd6e2
JiYoon <angella990825@gmail.com>
2023-10-31 16:58:47 +0900

    🔨 Refactor(#61): 신청서 제출 페이지 컴포넌트 화

--- src/app/(steady)/(application)/application/submit/[id]/page.tsx
+++ src/app/(steady)/(application)/application/submit/[id]/page.tsx
@@ -1,61 +1,83 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
-import { Separator } from "@radix-ui/themes";
+import { usePathname, useRouter } from "next/navigation";
+import ApplicationQuestion from "@/components/application/Question";
+import ApplicationTitle from "@/components/application/Title";
+import { TextArea } from "@radix-ui/themes";
 import Button, { buttonSize } from "@/components/_common/Button";
+
+const Application = {
+  id: "2", // 신청서 ID
+  item: [
+    {
+      question: "당신의 이름은?",
+      answer: "지윤",
+    },
+    {
+      question: "당신의 나이는?",
+      answer: "14",
+    },
+    {
+      question: "당신의 성별은?",
+      answer: "여",
+    },
+    {
+      question: "당신은 언제 언제 나오나요?",
+      answer: "일주일다",
+    },
+    {
+      question: "당신이 가장 잘하는 분야는?",
+      answer: "리액트, 넥스트, 타입스크립트",
+    },
+    {
+      question: "당신은 부지런한가요?",
+      answer: "넵",
+    },
+    {
+      question: "힘든 스터디일 수도 있습니다 버틸 수 있나요요?",
+      answer: "아니요",
+    },
+    {
+      question: "당신은 올빼미인가요? 종달새 인가요?",
+      answer: "둘당요",
+    },
+    {
+      question: "당신의 MBTI는?",
+      answer: "mbti",
+    },
+    {
+      question:
+        "세상에서 가장 긴 글을 작성하고 싶습니다 글자수를 제한해야 하지 않을까요?spspspspsfasfhasjkfhasfh",
+      answer:
+        "세상에서 가장 기느 답변을 작성하고 싶습니다 ㅇ로머롬너ㅏ롬너ㅗㄹ마ㅓ놀머ㅏ로어ㅏ로ㅓ암노러ㅏㅇㅁ로ㅓㅏㄴㅁ로ㅓㅏㄴ로ㅓㅁ나ㅗㄻ너ㅏ로ㅓㅏㄴ롬ㄴ롤",
+    },
+  ],
+};
 
 const SteadyPrimitive = {
   id: "2",
   title: "예감 스터디",
+  application: Application,
 };
 
-const Form = {
-  id: "2", // Form id
-  title: "예감 스터디 양식", // Form 제목
-  questions: [
-    "당신의 이름은?",
-    "당신의 나이는?",
-    "당신의 성별은?",
-    "당신은 언제 언제 나오나요?",
-    "당신이 가장 잘하는 분야는?",
-    "당신은 부지런한가요?",
-    "힘든 스터디일 수도 있습니다 버틸 수 있나요요?",
-    "당신은 올빼미인가요? 종달새 인가요?",
-    "당신의 MBTI는?",
-    "세상에서 가장 긴 글을 작성하고 싶습니다 글자수를 제한해야 하지 않을까요?spspspspsfasfhasjkfhasfh",
-  ], // Form 질문
-  createdAt: "2023.10.30", // Form 생성일
-  updatedAt: "2023.11.11", // Form 업데이트일
-};
-
-// TODO: 신청서 작성 성공시 Toast 성공 알림
+// TODO: 신청서 수정 성공시 Toast 성공 알림
 // TODO: react-hook-form과 ZOD로 유효성 검사
-const ApplicationSubmitPage = () => {
-  const params = useParams();
+
+const ApplicationEditPage = () => {
   const router = useRouter();
-  console.log(params.id); // 2
+  const pathname = usePathname();
+  const pageType = pathname.split("/")[2];
 
   return (
     <>
-      <div className="flex flex-row items-center gap-10 text-30 font-bold">
-        <div className=" text-st-primary">{SteadyPrimitive.title}</div>
-        <div>참여 신청서</div>
-      </div>
-      <Separator className="st-gray-400 h-3 w-auto" />
-      <div className="flex flex-col gap-30 px-100">
-        {Form.questions.map((question, id) => (
-          <div
-            key={id}
-            className="flex w-full flex-col gap-15 break-all"
-          >
-            <div className="flex gap-10 text-20 font-bold">
-              질문 {id + 1}.{question}
-            </div>
-            <textarea className="h-100 w-full rounded-12 border-3 border-st-skyblue-300" />
-          </div>
-        ))}
-      </div>
-      <Separator className="st-gray-400 h-3 w-auto" />
+      <ApplicationTitle
+        title={SteadyPrimitive.title}
+        pageType={pageType}
+      >
+        <ApplicationQuestion questions={SteadyPrimitive.application.item}>
+          <TextArea className="h-100 w-full" />
+        </ApplicationQuestion>
+      </ApplicationTitle>
       {/* TODO: 라우터 연결 */}
       <div className="flex items-center justify-end gap-20">
         <Button
@@ -68,11 +90,11 @@ const ApplicationSubmitPage = () => {
           onClick={() => router.push("/")}
           className={`${buttonSize.sm} bg-st-primary text-st-white`}
         >
-          작성 완료
+          수정 완료
         </Button>
       </div>
     </>
   );
 };
 
-export default ApplicationSubmitPage;
+export default ApplicationEditPage;
