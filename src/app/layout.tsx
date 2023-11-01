d184361bd7606da99465bd881c226cd55a25619a
Musix <ljw927@naver.com>
2023-11-01 22:22:04 +0900

    🚨 Fix(#83): Main Page 재구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body className="h-screen">
         <Theme className="h-full">
           <div
-            className={`max-mobile:w-9/10 mx-auto flex h-full w-3/4 flex-col pb-30`}
+            className={`max-mobile:w-9/10 mx-auto flex h-full w-3/4 flex-col items-center pb-30`}
           >
             <AppBar isLogin={false} />
             {children}
