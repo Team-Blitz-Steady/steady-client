2b68ae574b00b564e5a70c0a90c4d2faa6b1050a
Musix <ljw927@naver.com>
2023-11-01 22:24:17 +0900

    💄 Style(#84): 공통 컴포넌트 StickyButton 배경 색 추가

--- src/components/_common/StickyButton/index.tsx
+++ src/components/_common/StickyButton/index.tsx
@@ -9,7 +9,7 @@ const StickyButton = ({ onClick }: StickyButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="mobile:bottom-30 flex w-160 items-center justify-center rounded-full pb-5 pl-10 pr-2 pt-5 shadow-md"
+      className="mobile:bottom-30 flex w-160 items-center justify-center rounded-full bg-st-white pb-5 pl-10 pr-2 pt-5 shadow-md"
     >
       <div className="text-lg font-bold">문의하기</div>
       <Image
