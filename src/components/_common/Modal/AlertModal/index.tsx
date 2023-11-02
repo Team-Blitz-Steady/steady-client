01bc8ac1f56f014fcfc364c390f2013f4c8ddd10
JiYoon <angella990825@gmail.com>
2023-11-03 00:58:37 +0900

    💄 Style(#78): 알림창 모달 스타일 수정

--- src/components/_common/Modal/AlertModal/index.tsx
+++ src/components/_common/Modal/AlertModal/index.tsx
@@ -17,18 +17,18 @@ const AlertModal = ({
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>{trigger}</AlertDialog.Trigger>
-      <AlertDialog.Content className="flex h-250 w-400 items-center justify-center rounded-20 bg-st-primary max-mobile:h-1/4 max-mobile:w-3/4 max-mobile:p-10">
-        <div className="flex h-200 w-350 flex-col rounded-15 bg-st-white p-20 max-mobile:h-full max-mobile:w-full">
-          <div className="flex grow flex-col items-center justify-between">
+      <AlertDialog.Content className="max-mobile:h-1/4 max-mobile:w-3/4 max-mobile:p-10 flex h-250 w-400 items-center justify-center rounded-20 bg-st-primary">
+        <div className="max-mobile:h-full max-mobile:w-full flex h-200 w-350 flex-col rounded-15 bg-st-white p-20">
+          <div className="flex grow flex-col items-center justify-center">
             {children}
-            <div className="flex gap-20">
-              <AlertDialog.Cancel>
-                <Button className={`${buttonSize.sm} text-black bg-st-white `}>
-                  취소
-                </Button>
-              </AlertDialog.Cancel>
-              <AlertDialog.Action>{actionButton}</AlertDialog.Action>
-            </div>
+          </div>
+          <div className="flex items-center justify-center gap-20">
+            <AlertDialog.Cancel>
+              <Button className={`${buttonSize.sm} text-black bg-st-white `}>
+                취소
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>{actionButton}</AlertDialog.Action>
           </div>
         </div>
       </AlertDialog.Content>
