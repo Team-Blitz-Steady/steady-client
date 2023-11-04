587c97d1022ebf687086c2bd5c8a546c8a05b640
JiYoon <angella990825@gmail.com>
2023-11-04 16:06:59 +0900

    ✨ Feat(#96): 신청자 목록 및 답변 열람 페이지 레이아웃 구현

--- src/app/(steady)/(application)/applicant/layout.tsx
+++ src/app/(steady)/(application)/applicant/layout.tsx
@@ -1,26 +1,28 @@
 import { Separator } from "@radix-ui/themes";
+import Button, { buttonSize } from "@/components/_common/Button";
+import { AlertModal } from "@/components/_common/Modal";
 import SideBar from "@/components/_common/SideBar";
 
 const applicants = [
   {
     id: "1",
     label: "신청자1",
-    href: "/applicants/1",
+    href: "/applicant/1",
   },
   {
     id: "2",
     label: "신청자2",
-    href: "/applicants/2",
+    href: "/applicant/2",
   },
   {
     id: "3",
     label: "신청자3",
-    href: "/applicants/3",
+    href: "/applicant/3",
   },
   {
     id: "4",
     label: "신청자1",
-    href: "/applicants/1",
+    href: "/applicant/4",
   },
 ];
 
@@ -37,6 +39,36 @@ const ApplicantLayout = ({ children }: { children: React.ReactNode }) => {
         {children}
       </div>
       <Separator className="h-5 w-auto bg-st-gray-400" />
+      <div className="flex justify-end gap-10">
+        <AlertModal
+          trigger={
+            <Button className={`${buttonSize.sm} bg-st-red text-st-white`}>
+              거절
+            </Button>
+          }
+          actionButton={
+            <Button className={`${buttonSize.sm} bg-st-primary text-st-white`}>
+              승인
+            </Button>
+          }
+        >
+          <div className="text-18 font-bold">거절 하시겠습니까?</div>
+        </AlertModal>
+        <AlertModal
+          trigger={
+            <Button className={`${buttonSize.sm} bg-st-green text-st-white`}>
+              승인
+            </Button>
+          }
+          actionButton={
+            <Button className={`${buttonSize.sm} bg-st-primary text-st-white`}>
+              거절
+            </Button>
+          }
+        >
+          <div className="text-18 font-bold">승인 하시겠습니까?</div>
+        </AlertModal>
+      </div>
     </div>
   );
 };
