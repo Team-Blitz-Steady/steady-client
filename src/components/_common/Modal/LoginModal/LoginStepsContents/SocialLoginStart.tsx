a15f17540de789bb377428089ab80fda452f4335
JiYoon <angella990825@gmail.com>
2023-11-06 01:16:09 +0900

    ✨ Feat(#113): 카카오 로그인 테스트

--- src/components/_common/Modal/LoginModal/LoginStepsContents/SocialLoginStart.tsx
+++ src/components/_common/Modal/LoginModal/LoginStepsContents/SocialLoginStart.tsx
@@ -1,26 +1,18 @@
 "use client";
 
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import KaKaoLoginButton from "@/images/kakaobutton.png";
 import SteadyText from "@/images/steadytext.png";
 import SteadyTurtleIcon from "@/images/steadyturtle.png";
 
 // TODO: use client 뺄 수 있으면 빼기
 
-const REST_API_KEY = "3ed0efd25050442006de34536e7e8b80";
-const REDIRECT_URI = "http://localhost:3000";
-
 const SocialLoginStart = () => {
   // TODO: 백엔드에서 redirect uri 받기
   const handleClickKaKaoSocialLogin = () => {
-    const x = 500;
-    const y = 700;
-    const xPos = window.screen.width / 2 - x / 2; // 가운데 정렬
-    const yPos = window.screen.height / 2 - y / 2;
-    window.open(
-      `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
-      "스테디 카카오 소셜 로그인",
-      `width=${x},height=${y}" left=${xPos} top=${yPos}`,
+    redirect(
+      `https://kauth.kakao.com/oauth/authorize?client_id=b43ec3ba4cc2a54d31c6c1f7315c142b&redirect_uri=https://steady-client.vercel.app/&response_type=code`,
     );
   };
 
