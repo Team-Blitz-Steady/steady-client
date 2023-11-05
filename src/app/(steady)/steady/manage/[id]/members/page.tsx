2014dcdd68df8db86244cc938e2a63b6d6d240dd
JiYoon <angella990825@gmail.com>
2023-11-05 19:33:28 +0900

    ✨ Feat(#103): 기본 페이지 생성

---
+++ src/app/(steady)/steady/manage/[id]/members/page.tsx
@@ -0,0 +1,9 @@
+const SteadyManageMembersPage = () => {
+  return (
+    <div>
+      <h1>SteadyManagePage</h1>
+    </div>
+  );
+};
+
+export default SteadyManageMembersPage;
---
+++ src/app/(steady)/steady/manage/[id]/quiz/page.tsx
@@ -0,0 +1,5 @@
+const SteadyManageQuizPage = () => {
+  return <div>스테디 운영 퀴즈 페이지</div>;
+};
+
+export default SteadyManageQuizPage;
