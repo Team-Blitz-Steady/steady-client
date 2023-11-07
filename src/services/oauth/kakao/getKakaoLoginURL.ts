a5e01357381ee6f5a392ae884c8a205431e32caf
sscoderati <chungup5495@gmail.com>
2023-11-07 16:01:59 +0900

    📝 Chore(#113): getKakaoLogin 함수 내 반환 타입 명시
    - error 발생시 error 반환 코드 추가

--- src/services/oauth/kakao/getKakaoLoginURL.ts
+++ src/services/oauth/kakao/getKakaoLoginURL.ts
@@ -1,14 +1,17 @@
 import { baseInstance } from "@/services";
+import type { AxiosResponse } from "axios";
 
-const getKakaoLoginUrl = async () => {
+const getKakaoLoginUrl = async (): Promise<string> => {
   try {
-    const response = await baseInstance.get("/api/v1/auth/kakao");
+    const response: AxiosResponse<string> =
+      await baseInstance.get("/api/v1/auth/kakao");
     if (response.status / 10 !== 20) {
       throw new Error("Failed to fetch kakao url!");
     }
     return response.headers.location;
   } catch (error) {
     console.error(error);
+    throw error;
   }
 };
 
