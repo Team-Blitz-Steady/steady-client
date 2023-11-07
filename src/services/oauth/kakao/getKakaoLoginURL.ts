25ed18523a5a8b26f1693feb7dd4b8d503c9a647
sscoderati <chungup5495@gmail.com>
2023-11-07 16:00:06 +0900

    🚨 Fix(#113): 상태코드 판별 조건 수정

--- src/services/oauth/kakao/getKakaoLoginURL.ts
+++ src/services/oauth/kakao/getKakaoLoginURL.ts
@@ -3,7 +3,7 @@ import { baseInstance } from "@/services";
 const getKakaoLoginUrl = async () => {
   try {
     const response = await baseInstance.get("/api/v1/auth/kakao");
-    if (!(response.status === 201)) {
+    if (response.status / 10 !== 20) {
       throw new Error("Failed to fetch kakao url!");
     }
     return response.headers.location;
