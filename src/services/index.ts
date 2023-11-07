dbef27fc893e54b5a06b662b27b7fc1e7bf3bc4c
JiYoon <angella990825@gmail.com>
2023-11-07 18:38:46 +0900

    ✨ Feat(#122): 헤더에 토큰 넣는 interceptor 구현

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,23 +1,17 @@
 import type { AxiosRequestConfig } from "axios";
 import axios from "axios";
+import { getAccessToken } from "@/utils/cookies";
 
 // TODO: 환경변수 처리
 const BASE_URL = "https://dev.steadies.kr";
 
-const axiosApi = (url: string, options?: AxiosRequestConfig) => {
-  const instance = axios.create({ baseURL: url, ...options });
-  return instance;
-};
+const config: AxiosRequestConfig = { baseURL: BASE_URL };
+export const axiosInstance = axios.create(config);
 
-// const axiosAuthApi = (url: string, options?: AxiosRequestConfig) => {
-//   const token = "토큰 값";
-//   const instance = axios.create({
-//     baseURL: url,
-//     headers: { Authorization: "Bearer " + token },
-//     ...options,
-//   });
-//   return instance;
-// };
-
-export const baseInstance = axiosApi(BASE_URL);
-// export const authInstance = axiosAuthApi(BASE_URL)
+axiosInstance.interceptors.request.use(async (config) => {
+  const access_token = await getAccessToken();
+  if (access_token) {
+    config.headers.Authorization = `Bearer ${access_token.value}`;
+  }
+  return config;
+});
