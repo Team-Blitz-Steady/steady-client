fe835da03c7bea523a2e343e636074dd1142fa02
JiYoon <109654823+JIY00N2@users.noreply.github.com>
2023-11-08 16:48:45 +0900

    📝  Chore(#127): 에러 메시지 수정

--- src/services/steady/deleteSteady.ts
+++ src/services/steady/deleteSteady.ts
@@ -4,7 +4,7 @@ const deleteSteady = async (steadyId: string) => {
   try {
     const response = await axiosInstance.delete(`/api/v1/steadies/${steadyId}`);
     if (Math.floor(response.status / 10) !== 20) {
-      throw new Error("Failed to fetch steady delete url!");
+      throw new Error("Failed to fetch steady delete api!");
     }
     return response;
   } catch (error) {
