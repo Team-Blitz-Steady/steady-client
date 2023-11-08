64f2076bbe8f770b248873d4972ecebd5f2ca8e0
JiYoon <angella990825@gmail.com>
2023-11-08 14:28:27 +0900

    ✨ Feat(#126): services 폴더의 type 폴더에 타입 정의

--- src/services/steady/getSteadyDetails.ts
+++ src/services/steady/getSteadyDetails.ts
@@ -1,55 +1,6 @@
 import { axiosInstance } from "@/services";
 import type { AxiosResponse } from "axios";
-
-interface LeaderResponse {
-  id: number;
-  nickname: string;
-  profileImage: string;
-}
-
-interface Position {
-  id: number;
-  name: string;
-}
-
-interface Stack {
-  id: number;
-  name: string;
-  imageUrl: string;
-}
-
-interface SteadyDetailsType {
-  id: number;
-  leaderResponse: LeaderResponse;
-  name: string;
-  bio: string;
-  type: "STUDY" | "PROJECT";
-  status: "RECRUITING" | "CLOSED" | "FINISHED";
-  participantLimit: number;
-  numberOfParticipants: number;
-  steadyMode: "ONLINE" | "OFFLINE" | "BOTH";
-  scheduledPeriod:
-    | "TO_BE_DETERMINED"
-    | "ONE_WEEK"
-    | "TWO_WEEK"
-    | "THREE_WEEK"
-    | "FOUR_WEEK"
-    | "FIVE_WEEK"
-    | "TWO_MONTH"
-    | "THREE_MONTH"
-    | "FOUR_MONTH"
-    | "FIVE_MONTH"
-    | "SIX_MONTH"
-    | "LONG_TERM";
-  deadline: string;
-  title: string;
-  content: string;
-  positions: Position[];
-  stacks: Stack[];
-  isLeader: boolean;
-  isSubmittedUser: boolean;
-  promotionCount: number;
-}
+import type { SteadyDetailsType } from "../types";
 
 const getSteadyDetails = async (steadyId: number) => {
   try {
--- src/services/types/index.ts
+++ src/services/types/index.ts
@@ -6,3 +6,53 @@ export interface KakaoTokenType {
     refreshToken: string;
   };
 }
+
+export interface LeaderResponseType {
+  id: number;
+  nickname: string;
+  profileImage: string;
+}
+
+export interface PositionType {
+  id: number;
+  name: string;
+}
+
+export interface StackType {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+export interface SteadyDetailsType {
+  id: number;
+  leaderResponse: LeaderResponseType;
+  name: string;
+  bio: string;
+  type: "STUDY" | "PROJECT";
+  status: "RECRUITING" | "CLOSED" | "FINISHED";
+  participantLimit: number;
+  numberOfParticipants: number;
+  steadyMode: "ONLINE" | "OFFLINE" | "BOTH";
+  scheduledPeriod:
+    | "TO_BE_DETERMINED"
+    | "ONE_WEEK"
+    | "TWO_WEEK"
+    | "THREE_WEEK"
+    | "FOUR_WEEK"
+    | "FIVE_WEEK"
+    | "TWO_MONTH"
+    | "THREE_MONTH"
+    | "FOUR_MONTH"
+    | "FIVE_MONTH"
+    | "SIX_MONTH"
+    | "LONG_TERM";
+  deadline: string;
+  title: string;
+  content: string;
+  positions: PositionType[];
+  stacks: StackType[];
+  isLeader: boolean;
+  isSubmittedUser: boolean;
+  promotionCount: number;
+}
