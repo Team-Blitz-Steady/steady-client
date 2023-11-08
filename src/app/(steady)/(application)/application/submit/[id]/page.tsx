ab9a8be4c0eb2fce5ea66c1f4f6e23211371849d
JiYoon <angella990825@gmail.com>
2023-11-08 22:24:34 +0900

    ✨ Feat(#138): 신청서 제출 페이지 수정

--- src/app/(steady)/(application)/application/submit/[id]/page.tsx
+++ src/app/(steady)/(application)/application/submit/[id]/page.tsx
@@ -1,85 +1,86 @@
 "use client";
 
+import type { ChangeEvent } from "react";
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { Question, Title } from "@/components/application";
 import { TextArea } from "@radix-ui/themes";
+import submitApplication from "@/services/application/submitApplication";
+import type { SurveyType } from "@/services/types";
 import Button, { buttonSize } from "@/components/_common/Button";
 
-const Application = {
-  id: "2", // 신청서 ID
-  item: [
-    {
-      question: "당신의 이름은?",
-      answer: "지윤",
-    },
-    {
-      question: "당신의 나이는?",
-      answer: "14",
-    },
-    {
-      question: "당신의 성별은?",
-      answer: "여",
-    },
-    {
-      question: "당신은 언제 언제 나오나요?",
-      answer: "일주일다",
-    },
-    {
-      question: "당신이 가장 잘하는 분야는?",
-      answer: "리액트, 넥스트, 타입스크립트",
-    },
-    {
-      question: "당신은 부지런한가요?",
-      answer: "넵",
-    },
-    {
-      question: "힘든 스터디일 수도 있습니다 버틸 수 있나요요?",
-      answer: "아니요",
-    },
-    {
-      question: "당신은 올빼미인가요? 종달새 인가요?",
-      answer: "둘당요",
-    },
-    {
-      question: "당신의 MBTI는?",
-      answer: "mbti",
-    },
-    {
-      question:
-        "세상에서 가장 긴 글을 작성하고 싶습니다 글자수를 제한해야 하지 않을까요?spspspspsfasfhasjkfhasfh",
-      answer:
-        "세상에서 가장 기느 답변을 작성하고 싶습니다 ㅇ로머롬너ㅏ롬너ㅗㄹ마ㅓ놀머ㅏ로어ㅏ로ㅓ암노러ㅏㅇㅁ로ㅓㅏㄴㅁ로ㅓㅏㄴ로ㅓㅁ나ㅗㄻ너ㅏ로ㅓㅏㄴ롬ㄴ롤",
-    },
+const ApplicationQuestion = {
+  steadyId: "18",
+  questions: [
+    "당신의 이름은?",
+    "당신의 나이는?",
+    "당신의 성별은?",
+    "당신은 언제 언제 나오나요?",
+    "당신이 가장 잘하는 분야는?",
+    "당신은 부지런한가요?",
+    "힘든 스터디일 수도 있습니다 버틸 수 있나요요?",
+    "당신은 올빼미인가요? 종달새 인가요?",
+    "세상에서 가장 긴 글을 작성하고 싶습니다 글자수를 제한해야 하지 않을까요?spspspspsfasfhasjkfhasfh",
+    "당신의 mbti는?",
   ],
 };
 
-const SteadyPrimitive = {
-  id: "2",
-  title: "예감 스터디",
-  application: Application,
-};
-
+interface pageParams {
+  id: string;
+}
 // TODO: 신청서 수정 성공시 Toast 성공 알림
 // TODO: react-hook-form과 ZOD로 유효성 검사
 
-const ApplicationEditPage = () => {
+const ApplicationSubmitPage = ({ params }: { params: pageParams }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const steadyId = params.id;
   const pageType = pathname.split("/")[2];
+  const [applicationData, setApplicationData] = useState(
+    ApplicationQuestion.questions.map((question) => ({
+      question,
+      answer: "",
+    })),
+  );
+  useEffect(() => {
+    console.log(applicationData);
+  }, [applicationData]);
+
+  const handleChangeAnswer = (
+    event: ChangeEvent<HTMLTextAreaElement>,
+    id: number,
+  ) => {
+    const newAnswer = event.target.value;
+    const updatedApplicationData = [...applicationData];
+    updatedApplicationData[id].answer = newAnswer;
+    setApplicationData(updatedApplicationData);
+  };
+
+  const handleClickButton = async (
+    steadyId: string,
+    applicationData: SurveyType[],
+  ) => {
+    await submitApplication(steadyId, applicationData);
+    router.push("/");
+  };
 
   return (
     <>
       <Title
-        title={SteadyPrimitive.title}
+        title={"임스 스테디명"}
         pageType={pageType}
       >
-        {Application.item.map((item, id) => (
+        {applicationData.map(({ question, answer }, id) => (
           <Question
             key={id}
-            question={item.question}
+            question={question}
             index={id}
           >
-            <TextArea className="h-100 w-full" />
+            <TextArea
+              className="h-100 w-full"
+              value={answer}
+              onChange={(e) => handleChangeAnswer(e, id)}
+            />
           </Question>
         ))}
       </Title>
@@ -92,14 +93,14 @@ const ApplicationEditPage = () => {
           취소
         </Button>
         <Button
-          onClick={() => router.push("/")}
+          onClick={() => handleClickButton(steadyId, applicationData)}
           className={`${buttonSize.sm} bg-st-primary text-st-white`}
         >
-          수정 완료
+          제출 완료
         </Button>
       </div>
     </>
   );
 };
 
-export default ApplicationEditPage;
+export default ApplicationSubmitPage;
