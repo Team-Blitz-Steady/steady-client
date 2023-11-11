bb2b4e59fd494b1d49eb584b181ea2238c9a48ed
JiYoon <angella990825@gmail.com>
2023-11-12 02:00:34 +0900

    ✨ Feat(#155): 스테디 상세 설명 시작 온보딩 구현

--- src/components/_common/Modal/LoginModal/LoginStepsContents/SteadyDescriptionStart.tsx
+++ src/components/_common/Modal/LoginModal/LoginStepsContents/SteadyDescriptionStart.tsx
@@ -1,18 +1,27 @@
+import useLoginStepsStore from "@/stores/loginSteps";
+import Button, { buttonSize } from "@/components/_common/Button";
 import { loginTextStyles } from "./SetNickname";
 
 const SteadyDescriptionStart = () => {
+  const { setIncreaseSteps } = useLoginStepsStore();
   return (
-    <div className="flex h-full w-full flex-col items-center justify-evenly">
-      <div className="flex flex-col items-center justify-evenly gap-10">
+    <>
+      <div className="flex h-full w-full flex-col items-center justify-evenly">
         <div
-          className={`${loginTextStyles} leading-1000 max-mobile:w-auto w-5/6 text-center`}
+          className={`${loginTextStyles} leading-1000 max-mobile:w-auto text-center`}
         >
-          <span className="text-st-primary">{'"스테디"'}</span> 는
-          <div>스터디와 프로젝트를</div>
-          <div> 모두 포함하는 용어입니다 !</div>
+          <span className="text-st-primary">{'"스테디"'}</span>
+          <span>는 스터디와 프로젝트를</span>
+          <div> 모두 포함하는 저희 서비스 용어입니다!</div>
         </div>
       </div>
-    </div>
+      <Button
+        className={`${buttonSize.md} bg-st-primary text-st-white`}
+        onClick={() => setIncreaseSteps()}
+      >
+        다음
+      </Button>
+    </>
   );
 };
 
