cbef40c8cae28265fcf09fc1d3a45c80abef28fc
JiYoon <angella990825@gmail.com>
2023-11-12 20:04:25 +0900

    ✨ Feat(#155): 닉네임 스키마 수정

--- src/schemas/setNickname.ts
+++ src/schemas/setNickname.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-const REGEX = /^[a-zA-Z가-힣0-9]+$/;
+const REGEX = /^[a-zA-Z가-힣0-9 ]+$/;
 
 export type NicknameSchemaType = z.infer<typeof nicknameSchema>;
 export type PositionAndStacksSchemaType = z.infer<
@@ -10,10 +10,13 @@ export type PositionAndStacksSchemaType = z.infer<
 export const nicknameSchema = z.object({
   nickname: z
     .string()
-    .min(2, { message: "닉네임을 2글자 이상 입력해주세요!" })
-    .max(15, { message: "닉네임을 15글자 이하로 입력해주세요!" })
-    .trim()
-    .regex(REGEX, {
+    .refine((value) => value.trim().length >= 2 && value.trim().length <= 15, {
+      message: "닉네임을 2글자 이상 15글자 이하로 입력해주세요!",
+    })
+    .refine((value) => /\S/.test(value), {
+      message: "닉네임은 양 쪽 공백을 제외하고 문자로 구성되어야 합니다!",
+    })
+    .refine((value) => REGEX.test(value), {
       message: "닉네임은 한글, 영문, 숫자만 가능합니다!",
     }),
 });
