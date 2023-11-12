c7c81119e42e7a4800baecd12697d74fb7b217e1
JiYoon <angella990825@gmail.com>
2023-11-12 22:43:46 +0900

    📝 Chore(#155): 타입스크립트 오류로 인해 주석 처리

--- src/utils/extractValue.ts
+++ src/utils/extractValue.ts
@@ -1,12 +1,13 @@
 // thanks to @Souperman at https://stackoverflow.com/questions/73825273/creating-a-zod-enum-from-an-object
 export const extractValue = (target: { value: string; label: string }[]) => {
-  if (
-    !target ||
-    target.length === 0 ||
-    target.some((item) => item === undefined || item.value === undefined)
-  ) {
-    return [];
-  }
+  // TODO: 타입스크립트 오류 해결
+  // if (
+  //   !target ||
+  //   target.length === 0 ||
+  //   target.some((item) => item === undefined || item.value === undefined)
+  // ) {
+  //   return [];
+  // }
   type property = (typeof target)[number]["value"];
   const valueArray: [property, ...property[]] = [
     target[0].value,
