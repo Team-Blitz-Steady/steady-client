d9523dd7616fbb8f5046666f619911dc856d366e
Musix <ljw927@naver.com>
2023-11-12 23:49:31 +0900

    📝 Chore(#120): build 오류 주석 처리

--- src/utils/extractValue.ts
+++ src/utils/extractValue.ts
@@ -1,12 +1,12 @@
 // thanks to @Souperman at https://stackoverflow.com/questions/73825273/creating-a-zod-enum-from-an-object
 export const extractValue = (target: { value: string; label: string }[]) => {
-  if (
-    !target ||
-    target.length === 0 ||
-    target.some((item) => item === undefined || item.value === undefined)
-  ) {
-    return [];
-  }
+  // if (
+  //   !target ||
+  //   target.length === 0 ||
+  //   target.some((item) => item === undefined || item.value === undefined)
+  // ) {
+  //   return [];
+  // }
   type property = (typeof target)[number]["value"];
   const valueArray: [property, ...property[]] = [
     target[0].value,
