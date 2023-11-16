844cc0e1f8af791bb26b00c9d81657bce6eaa875
JiYoon <angella990825@gmail.com>
2023-11-16 23:42:52 +0900

    ✨ Feat(#171): 신청서 작성 페이지 구현

--- src/app/(steady)/(application)/application/submit/[id]/page.tsx
+++ src/app/(steady)/(application)/application/submit/[id]/page.tsx
@@ -1,100 +1,127 @@
 "use client";
 
 import type { ChangeEvent } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { Question, Title } from "@/components/application";
+import { useToast } from "@/components/ui/use-toast";
 import { TextArea } from "@radix-ui/themes";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import submitApplication from "@/services/application/submitApplication";
-import type { ApplicationSurveyType } from "@/services/types";
+import getSteadyQuestions from "@/services/steady/getSteadyQuestions";
 import Button, { buttonSize } from "@/components/_common/Button";
-
-const ApplicationQuestion = {
-  steadyId: "18",
-  questions: [
-    "당신의 이름은?",
-    "당신의 나이는?",
-    "당신의 성별은?",
-    "당신은 언제 언제 나오나요?",
-    "당신이 가장 잘하는 분야는?",
-    "당신은 부지런한가요?",
-    "힘든 스터디일 수도 있습니다 버틸 수 있나요요?",
-    "당신은 올빼미인가요? 종달새 인가요?",
-    "세상에서 가장 긴 글을 작성하고 싶습니다 글자수를 제한해야 하지 않을까요?spspspspsfasfhasjkfhasfh",
-    "당신의 mbti는?",
-  ],
-};
 
 interface pageParams {
   id: string;
 }
-// TODO: 신청서 수정 성공시 Toast 성공 알림
-// TODO: react-hook-form과 ZOD로 유효성 검사
 
 const ApplicationSubmitPage = ({ params }: { params: pageParams }) => {
   const router = useRouter();
   const pathname = usePathname();
   const steadyId = params.id;
   const pageType = pathname.split("/")[2];
+  const { data: steadyQuestionsData } = useSuspenseQuery({
+    queryKey: ["steadyQuestionsSubmit"],
+    queryFn: () => getSteadyQuestions(steadyId),
+  });
   const [applicationData, setApplicationData] = useState(
-    ApplicationQuestion.questions.map((question) => ({
-      question,
+    steadyQuestionsData.steadyQuestions.map((item) => ({
+      question: item.content,
       answer: "",
     })),
   );
+  const { toast } = useToast();
+  const [isSubmit, setIsSubmit] = useState(false);
+  // isSubmit한사람이면 replace 상세페이지
+
+  useEffect(() => {
+    if (isSubmit) {
+      router.replace(`/steady/detail/${steadyId}`);
+    }
+  }, [isSubmit, router, steadyId]);
 
   const handleChangeAnswer = (
     event: ChangeEvent<HTMLTextAreaElement>,
-    id: number,
+    index: number,
   ) => {
     const newAnswer = event.target.value;
-    const updatedApplicationData = [...applicationData];
-    updatedApplicationData[id].answer = newAnswer;
-    setApplicationData(updatedApplicationData);
+    setApplicationData((prevData) =>
+      prevData.map((question, id) =>
+        id === index ? { ...question, answer: newAnswer } : question,
+      ),
+    );
   };
 
-  const handleClickButton = async (
-    steadyId: string,
-    applicationData: ApplicationSurveyType[],
-  ) => {
-    await submitApplication(steadyId, applicationData);
-    router.push(`/steady/detail/${steadyId}`);
+  const onSubmitAnswers = async () => {
+    const checkInvalidAnswers = applicationData.some(
+      (item) => !item.answer.length,
+    );
+    if (checkInvalidAnswers) {
+      toast({
+        description: "입력되지 않은 항목이 있습니다.",
+        variant: "red",
+      });
+    } else {
+      try {
+        await submitApplication(steadyId, applicationData);
+        toast({
+          description: "스테디 참여 신청서 제출 성공!",
+          variant: "green",
+        });
+        setIsSubmit(true);
+      } catch (error) {
+        toast({
+          description: "스테디 참여 신청서 제출 실패!",
+          variant: "red",
+        });
+      }
+    }
   };
 
   return (
     <>
       <Title
-        title={"임스 스테디명"}
+        title={steadyQuestionsData.steadyName}
         pageType={pageType}
       >
-        {applicationData.map(({ question, answer }, id) => (
+        {applicationData.map((item, index) => (
           <Question
-            key={id}
-            question={question}
-            index={id}
+            key={index}
+            question={item.question}
+            index={index}
           >
             <TextArea
               className="h-100 w-full"
-              value={answer}
-              onChange={(event) => handleChangeAnswer(event, id)}
+              value={item.answer}
+              onChange={(event) => {
+                handleChangeAnswer(event, index);
+              }}
             />
+            {!item.answer.trim() && (
+              <span className="text-15 text-st-red">
+                {"해당 항목을 입력해주세요"}
+              </span>
+            )}
           </Question>
         ))}
       </Title>
-      {/* TODO: 라우터 연결 */}
       <div className="flex items-center justify-end gap-20">
-        <Button
-          className={`${buttonSize.sm} bg-st-white`}
-          onClick={() => router.back()}
-        >
-          취소
-        </Button>
-        <Button
-          onClick={() => handleClickButton(steadyId, applicationData)}
-          className={`${buttonSize.sm} bg-st-primary text-st-white`}
-        >
-          제출 완료
-        </Button>
+        {!isSubmit && (
+          <>
+            <Button
+              className={`${buttonSize.sm} bg-st-white`}
+              onClick={() => router.back()}
+            >
+              취소
+            </Button>
+            <Button
+              onClick={onSubmitAnswers}
+              className={`${buttonSize.sm} bg-st-primary text-st-white`}
+            >
+              제출 완료
+            </Button>
+          </>
+        )}
       </div>
     </>
   );
