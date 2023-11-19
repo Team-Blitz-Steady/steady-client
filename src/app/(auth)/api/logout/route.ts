0b784958a03a2cf4320aff9e7653f8d9bc39b153
sscoderati <chungup5495@gmail.com>
2023-11-19 14:44:02 +0900

    🚨 Fix(#207): 로그아웃 실패 시 직접 쿠키 만료 헤더 응답 전송

--- src/app/(auth)/api/logout/route.ts
+++ src/app/(auth)/api/logout/route.ts
@@ -7,8 +7,21 @@ export async function GET() {
     cookies().delete("refresh_token");
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: "Logout failed!" });
+    return NextResponse.json(
+      { message: "Logout failed! Delete Cookies Manually..." },
+      {
+        status: 200,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+        headers: {
+          "content-type": "application/json",
+          "Set-Cookie": [
+            "access_token=; Path=/; Max-Age=0",
+            "refresh_token=; Path=/; Max-Age=0",
+          ],
+        },
+      },
+    );
   }
-
   return NextResponse.json({ message: "Logout success!" });
 }
