6390981fc6c476fd4dfb08b5c8ad6a05f3d17fb5
JiYoon <angella990825@gmail.com>
2023-11-22 02:07:02 +0900

    ✨ Feat(#188): 쿠키를 클라이언트, 서버 구분 없이 사용 가능한 서비스 객체 추가

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 import type { AxiosRequestConfig } from "axios";
 import axios from "axios";
-import { getCookie } from "cookies-next";
 
 // TODO: 환경변수 처리
 const BASE_URL = "https://dev.steadies.kr";
@@ -9,16 +8,38 @@ const config: AxiosRequestConfig = { baseURL: BASE_URL };
 export const axiosInstance = axios.create(config);
 
 axiosInstance.interceptors.request.use(async (config) => {
-  let access_token = null;
-  if (typeof window !== "undefined") {
-    access_token = getCookie("access_token");
-  } else {
-    const { cookies } = await import("next/headers");
-    access_token = cookies().get("access_token");
-  }
+  const access_token = await cookieService.get("access_token");
   console.log("access_token은!!", access_token);
   if (access_token) {
     config.headers.Authorization = `Bearer ${access_token}`;
   }
   return config;
 });
+
+export const cookieService = {
+  get: async (key: string) => {
+    if (typeof window === "undefined") {
+      const { cookies } = await import("next/headers");
+      return cookies().get(key);
+    }
+    const { getCookie: get_cookie } = await import("cookies-next");
+    return get_cookie(key);
+  },
+  set: async (key: string, value: string) => {
+    if (typeof window === "undefined") {
+      const { cookies } = await import("next/headers");
+      return cookies().set(key, value);
+    }
+    const { setCookie: set_cookie } = await import("cookies-next");
+    return set_cookie(key, value);
+  },
+};
+
+// export const getCookie = async (key: string) => {
+//   if (typeof window === "undefined") {
+//     const { cookies } = await import("next/headers");
+//     return cookies().get(key);
+//   }
+//   const { getCookie: get_cookie } = await import("cookies-next");
+//   return get_cookie(key);
+// };
