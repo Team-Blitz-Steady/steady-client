2963b6620ec16769860e00db32a78877ed43cb51
sscoderati <chungup5495@gmail.com>
2023-11-23 14:43:18 +0900

    🚨 Fix(#47): Route Handler CORS 헤더 추가

--- src/app/(auth)/api/login/route.ts
+++ src/app/(auth)/api/login/route.ts
@@ -2,6 +2,12 @@ import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function POST(req: NextRequest) {
   try {
     const { token } = await req.json();
@@ -15,7 +21,13 @@ export async function POST(req: NextRequest) {
     console.error(error);
   }
 
-  return NextResponse.json({
-    message: "login success!",
-  });
+  return NextResponse.json(
+    {
+      message: "login success!",
+    },
+    {
+      status: 200,
+      headers: corsHeaders,
+    },
+  );
 }
