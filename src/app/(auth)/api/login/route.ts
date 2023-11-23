6328cd41b9459433af20c1db69982a07684ca6cd
sscoderati <chungup5495@gmail.com>
2023-11-23 20:36:07 +0900

    🚨 Fix(#47): vercel.json 삭제 및 핸들러 반환 응답 설정 변경

--- src/app/(auth)/api/login/route.ts
+++ src/app/(auth)/api/login/route.ts
@@ -1,23 +1,10 @@
 import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
 
 export async function POST(req: NextRequest) {
   try {
-    req.headers.set("Content-Type", "application/json");
-    req.headers.append("Access-Control-Allow-Credentials", "true");
-    req.headers.append(
-      "Access-Control-Allow-Origin",
-      "https://www.steadies.kr",
-    );
-    req.headers.append(
-      "Access-Control-Allow-Methods",
-      "GET,DELETE,PATCH,POST,PUT",
-    );
-    req.headers.append(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    );
     const { token } = await req.json();
     cookies().set("access_token", token.access, {
       maxAge: 60 * 60 * 23, // 23 hours
@@ -29,11 +16,12 @@ export async function POST(req: NextRequest) {
     console.error(error);
   }
 
-  if (req.method === "OPTIONS") {
-    return NextResponse.json({
-      status: 200,
-    });
-  }
-
-  return NextResponse.json({ result: "Ok" }, { status: 200 });
+  return new Response("Login success!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://www.steadies.kr",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
