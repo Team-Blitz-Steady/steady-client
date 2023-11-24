0c282c64050a6516bf768843d7dc23ceb99a5bba
sscoderati <chungup5495@gmail.com>
2023-11-24 15:10:51 +0900

    ✨ Feat(#239): 회원 탈퇴 api 요청 함수 추가

---
+++ src/services/user/deleteMyProfile.ts
@@ -0,0 +1,16 @@
+import { axiosInstance, isAbnormalCode } from "..";
+
+const deleteMyProfile = async () => {
+  try {
+    const response = await axiosInstance.delete("/api/v1/user");
+    if (isAbnormalCode(response.status)) {
+      throw new Error("Failed to fetch delete my profile api!");
+    }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export default deleteMyProfile();
