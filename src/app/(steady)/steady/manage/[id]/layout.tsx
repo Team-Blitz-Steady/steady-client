510d212921fbbe978e71d7424617162855d3fd3f
sscoderati <chungup5495@gmail.com>
2023-11-24 23:24:43 +0900

    📝 Chore(#103): 좌측 사이드바 삭제 및 레이아웃 수정

--- src/app/(steady)/steady/manage/[id]/layout.tsx
+++ src/app/(steady)/steady/manage/[id]/layout.tsx
@@ -2,38 +2,26 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 import { Separator } from "@radix-ui/themes";
 import Button, { buttonSize } from "@/components/_common/Button";
 import { AlertModal } from "@/components/_common/Modal";
-import SideBar from "@/components/_common/SideBar";
-
-const manageItems = [
-  {
-    label: "스테디 멤버 관리",
-    href: "/steady/manage/1/members",
-  },
-  {
-    label: "스테디 퀴즈",
-    href: "/steady/manage/1/quiz",
-  },
-  {
-    label: "스테디 공지",
-    href: "/steady/manage/1/notice",
-  },
-];
 
 const SteadyManageLayout = ({ children }: { children: React.ReactNode }) => {
   const path = usePathname();
   const id = path.split("/")[3];
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex w-1000 flex-col">
       <div className="mb-20 flex items-center justify-between">
         {/* TODO: 스테디명 받기 */}
         <div className="text-30 font-bold">{"{스테디명} 운영"} </div>
         <div className="flex gap-20">
           <Link href={`/steady/applicant/${id}`}>
             <Button
-              className={`${buttonSize.md} bg-st-primary text-16 text-st-white`}
+              className={`${cn(
+                buttonSize.sm,
+                "w-130 bg-st-primary text-16 text-st-white",
+              )}`}
             >
               신청자 보기
             </Button>
@@ -41,7 +29,10 @@ const SteadyManageLayout = ({ children }: { children: React.ReactNode }) => {
           <AlertModal
             trigger={
               <Button
-                className={`${buttonSize.md} bg-st-red text-16 text-st-white`}
+                className={`${cn(
+                  buttonSize.sm,
+                  "w-130 bg-st-red text-16 text-st-white",
+                )}`}
               >
                 스테디 종료
               </Button>
@@ -61,16 +52,7 @@ const SteadyManageLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </div>
       <Separator className="h-5 w-auto bg-st-gray-400" />
-      <div className="flex w-full flex-row gap-30 py-30">
-        <div className="w-fit">
-          <SideBar
-            listType="manage"
-            sidebarItems={manageItems}
-          />
-        </div>
-        {children}
-      </div>
-      <Separator className="h-5 w-auto bg-st-gray-400" />
+      {children}
     </div>
   );
 };
