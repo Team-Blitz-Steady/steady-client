bbe24d520a4f4c16538aaaa678eee6372eef414b
sscoderati <chungup5495@gmail.com>
2023-11-24 23:23:03 +0900

    🗑️ Remove(#103): 사용하지 않는 페이지 삭제
    - 스테디 운영: 공지 페이지
    - 스테디 운영: 퀴즈 페이지

--- src/app/(steady)/steady/manage/[id]/notice/page.tsx
+++
@@ -1,5 +0,0 @@
-const SteadyManageNoticePage = () => {
-  return <div>스테디 운영 공지 페이지</div>;
-};
-
-export default SteadyManageNoticePage;
--- src/app/(steady)/steady/manage/[id]/quiz/page.tsx
+++
@@ -1,5 +0,0 @@
-const SteadyManageQuizPage = () => {
-  return <div>스테디 운영 퀴즈 페이지</div>;
-};
-
-export default SteadyManageQuizPage;
