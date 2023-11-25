c5d32d7d8d01b67e50e34effe7a3e3b3e1368129
JiYoon <angella990825@gmail.com>
2023-11-26 00:53:36 +0900

    ✨ Feat(#252): 로그인 온보딩 추가

---
+++ src/components/_common/Modal/LoginModal/LoginStepsContents/SelectedSteadyAction.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const SelectedSteadyAction = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-evenly">
+      <div className="flex w-full flex-col items-center justify-center gap-10">
+        <div className="text-25 font-bold">🧐 스테디에 참여하고 싶은 사람</div>
+        <Link
+          href={"/"}
+          className="h-200 w-full"
+        >
+          <button className="text-black h-full w-full rounded-10 text-30 font-bold hover:bg-st-skyblue-50">
+            🔎 스테디 검색
+          </button>
+        </Link>
+      </div>
+      <div className="flex w-full flex-col items-center justify-center gap-10">
+        <div className="text-25 font-bold">🤗 스테디를 운영하고 싶은 사람</div>
+        <Link
+          href={"/steady/create"}
+          className="h-200 w-full"
+        >
+          <button className="text-black h-full w-full rounded-10 text-30 font-bold hover:bg-st-skyblue-50">
+            ✍️ 스테디 생성
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SelectedSteadyAction;
