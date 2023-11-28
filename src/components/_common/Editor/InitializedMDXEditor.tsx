789ef9f31b674e591c8dea2eaaf7a1284af51ddf
sscoderati <chungup5495@gmail.com>
2023-11-28 14:56:25 +0900

    ✨ Feat(#261): 마크다운 에디터 기본 설정

---
+++ src/components/_common/Editor/InitializedMDXEditor.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+// InitializedMDXEditor.tsx
+import type { ForwardedRef } from "react";
+import {
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  MDXEditor,
+  type MDXEditorMethods,
+  type MDXEditorProps,
+  Separator,
+  UndoRedo,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  headingsPlugin,
+  imagePlugin,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  quotePlugin,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+} from "@mdxeditor/editor";
+
+// Only import this to the next file
+export default function InitializedMDXEditor({
+  editorRef,
+  ...props
+}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+  return (
+    <MDXEditor
+      plugins={[
+        // Example Plugin Usage
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        imagePlugin(),
+        tablePlugin(),
+        markdownShortcutPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "txt" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: "JavaScript",
+            css: "CSS",
+            txt: "text",
+          },
+        }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <ListsToggle />
+              <Separator />
+              <BlockTypeSelect />
+              <Separator />
+            </>
+          ),
+        }),
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  );
+}
