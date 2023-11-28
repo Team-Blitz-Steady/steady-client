040c3c6dbf59b0dbb6c2049f1dc6cd5fc3f643d7
JiYoon <angella990825@gmail.com>
2023-11-29 01:43:58 +0900

    💄 Style(#282): 신청자 목록 및 답변 열람 페이지 반응형 구현

--- src/app/(steady)/steady/applicant/[steady_id]/layout.tsx
+++ src/app/(steady)/steady/applicant/[steady_id]/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { Suspense, useState } from "react";
+import { Fragment, Suspense, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,55 +25,54 @@ const SteadyApplicantLayout = ({
   const { applicantListData, hasNextPage, fetchNextPage } =
     useApplicantListQuery({ steadyId });
 
+  const applicantList = applicantListData.pages.flatMap((page) => page.content);
+
   return (
     <>
       <div className="w-fit">
-        <div className="flex h-850 w-250 flex-col items-center gap-15 overflow-y-auto overflow-x-hidden rounded-20 border-1 border-solid border-st-gray-100 p-20 scrollbar-hide">
-          <InfiniteScroll
-            hasMore={hasNextPage}
-            loadMore={() => fetchNextPage()}
-            useWindow={false}
-          >
-            {applicantListData.pages.map((page) =>
-              page.content.map((user, id) => (
-                <div key={user.userId}>
-                  <div
-                    className={`flex w-200 items-center gap-10 rounded-5 p-20 text-18 font-bold transition duration-100 ${
-                      selectedItem === id
-                        ? selectedEffectStyle
-                        : normalEffectStyle
-                    }`}
-                    onClick={() => setSelectedItem(id)}
-                  >
-                    <UserModal
-                      trigger={
-                        <div>
-                          <Image
-                            className="cursor-pointer rounded-full border-1"
-                            src={user.profileImage}
-                            alt="유저 프로필 이미지"
-                            width={50}
-                            height={50}
-                          />
-                        </div>
-                      }
-                    >
-                      <Suspense fallback={<Spinner size="medium" />}>
-                        <UserItems userId={user.userId} />
-                      </Suspense>
-                    </UserModal>
-                    <Link
-                      href={`/steady/applicant/${steadyId}/${user.applicationId}`}
-                      className="w-full"
-                    >
-                      <div>{user.nickname}</div>
-                    </Link>
-                  </div>
-                </div>
-              )),
-            )}
-          </InfiniteScroll>
-        </div>
+        <InfiniteScroll
+          hasMore={hasNextPage}
+          loadMore={() => fetchNextPage()}
+          useWindow={false}
+          className="flex h-650 flex-col items-center gap-10 overflow-y-auto overflow-x-hidden rounded-20 border-1 border-solid border-st-gray-100 p-20 scrollbar-hide max-sm:w-150 sm:w-150 md:w-180 lg:w-200 xl:w-250"
+        >
+          {applicantList.map((user, id) => (
+            <Fragment key={user.userId}>
+              <div
+                className={`flex items-center gap-10 rounded-5 p-15 text-18 font-bold transition duration-100 max-sm:w-120 sm:w-120 md:w-140 lg:w-170 xl:w-220 ${
+                  selectedItem === id ? selectedEffectStyle : normalEffectStyle
+                }`}
+                onClick={() => setSelectedItem(id)}
+              >
+                <UserModal
+                  trigger={
+                    <div>
+                      <Image
+                        className="cursor-pointer rounded-full border-1"
+                        src={user.profileImage}
+                        alt="유저 프로필 이미지"
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                  }
+                >
+                  <Suspense fallback={<Spinner size="large" />}>
+                    <UserItems userId={user.userId} />
+                  </Suspense>
+                </UserModal>
+                <Link
+                  href={`/steady/applicant/${steadyId}/${user.applicationId}`}
+                  className="w-full"
+                >
+                  <span className="line-clamp-1 max-sm:text-12 sm:text-14 md:text-16 lg:text-18 xl:text-20">
+                    {user.nickname}
+                  </span>
+                </Link>
+              </div>
+            </Fragment>
+          ))}
+        </InfiniteScroll>
       </div>
       {children}
     </>
