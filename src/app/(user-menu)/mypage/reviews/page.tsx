288222d5058dafb505e6c80d7f2adc2ece2801e8
JiYoon <angella990825@gmail.com>
2023-11-29 00:28:17 +0900

    💄 Style(#280): 내 리뷰 페이지 반응형 구현

--- src/app/(user-menu)/mypage/reviews/page.tsx
+++ src/app/(user-menu)/mypage/reviews/page.tsx
@@ -8,7 +8,7 @@ import changeReviewStatus from "@/services/review/changeReviewStatus";
 import getMyReviews from "@/services/review/getMyReviews";
 import Icon from "@/components/_common/Icon";
 import { SingleSelector } from "@/components/_common/Selector";
-import { subMyPageTextStyles } from "@/constants/commonStyle";
+import { subBoxStyles, subMyPageTextStyles } from "@/constants/commonStyle";
 import { MyReviewKey } from "@/constants/queryKeys";
 
 const MyReviewsPage = () => {
@@ -30,15 +30,13 @@ const MyReviewsPage = () => {
   };
 
   return (
-    <div className="flex h-full w-fit flex-col gap-100">
-      <div className="text-30 font-bold">내가 받은 리뷰</div>
+    <div className="flex h-full flex-col gap-50 max-sm:w-400 sm:w-500 md:w-400 lg:w-600 xl:w-750">
+      <div className="font-bold max-sm:text-22 sm:text-22 md:text-25 lg:text-28 xl:text-30">
+        내가 받은 리뷰
+      </div>
       <div className={cn("flex flex-col gap-30")}>
         <div className={cn(subMyPageTextStyles.title)}>받은 카드</div>
-        <div
-          className={cn(
-            "flex h-116 w-718 items-center justify-evenly rounded-6 border-2 border-st-gray-100 px-30 py-20",
-          )}
-        >
+        <div className={cn(subBoxStyles, "justify-evenly")}>
           {myReviewData.userCards.map((card) => (
             <div
               key={card.cardId}
@@ -60,7 +58,7 @@ const MyReviewsPage = () => {
           ))}
         </div>
       </div>
-      <div className={cn("flex w-718 flex-col gap-30")}>
+      <div className={cn("flex w-full flex-col gap-30")}>
         <div className={cn(subMyPageTextStyles.title)}>한 줄 평</div>
         <SingleSelector
           items={myReviewData.reviews.map((review) => ({
