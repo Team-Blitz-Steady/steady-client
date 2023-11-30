e03f71e2fe8d50f77465f521cd48f466ec37baea
JiYoon <angella990825@gmail.com>
2023-11-30 14:29:00 +0900

    💄 Style(#286): 스테디 운영 페이지 반응형 구현

--- src/app/(steady)/steady/manage/[id]/page.tsx
+++ src/app/(steady)/steady/manage/[id]/page.tsx
@@ -122,17 +122,18 @@ const SteadyManagePage = ({ params }: { params: { id: string } }) => {
   }
 
   return (
-    <div className="flex w-1000 flex-col">
-      <div className="mb-20 flex items-center justify-between">
-        <div className="text-30 font-bold">
-          {`${steadyDetailsData.name} 운영 페이지`}
+    <div className="flex flex-col max-sm:w-400 sm:w-450 md:w-600 lg:w-800 xl:w-1000">
+      <div className="mb-10 flex w-full items-center justify-between">
+        <div className="font-bold max-sm:text-22 sm:text-22 md:text-25 lg:text-28 xl:text-30">
+          <span className="text-st-primary">{steadyDetailsData.name}</span>
+          <span> 스테디 운영</span>
         </div>
-        <div className="flex gap-20">
+        <div className="flex gap-10">
           <Link href={`/steady/applicant/${steadyId}`}>
             <Button
               className={`${cn(
                 buttonSize.sm,
-                "w-130 bg-st-primary text-16 text-st-white",
+                "bg-st-primary text-st-white max-sm:w-70 sm:w-80 md:w-90 lg:w-100 xl:w-130",
               )}`}
             >
               신청자 보기
@@ -143,7 +144,7 @@ const SteadyManagePage = ({ params }: { params: { id: string } }) => {
               <Button
                 className={`${cn(
                   buttonSize.sm,
-                  "w-130 bg-st-red text-16 text-st-white",
+                  " bg-st-red  text-st-white max-sm:w-70 sm:w-80 md:w-90 lg:w-100 xl:w-130",
                 )}`}
               >
                 스테디 종료
@@ -167,7 +168,7 @@ const SteadyManagePage = ({ params }: { params: { id: string } }) => {
               <Button
                 className={`${cn(
                   buttonSize.sm,
-                  "w-130 bg-st-red text-16 text-st-white",
+                  "bg-st-red text-st-white max-sm:w-70 sm:w-80 md:w-90 lg:w-100 xl:w-130 ",
                 )}`}
               >
                 스테디 삭제
@@ -188,189 +189,146 @@ const SteadyManagePage = ({ params }: { params: { id: string } }) => {
           </AlertModal>
         </div>
       </div>
-      <Separator className="h-5 w-auto bg-st-gray-400" />
-      <div className="flex overflow-x-hidden">
-        <Tabs.Root
-          defaultValue={`${steadyParticipantsData.participants[0].id}`}
-        >
-          <Tabs.List className="flex w-1000 gap-10">
-            {steadyParticipantsData.participants.map((participant) => (
-              <Tabs.Trigger
-                key={participant.id}
-                value={participant.id.toString()}
-                className="cursor-pointer"
-                onClick={() => {
-                  setCurrentUserIndex(
-                    steadyParticipantsData.participants.findIndex(
-                      (item) => item.id === participant.id,
-                    ),
-                  );
-                }}
-              >
-                {participant.nickname}
-              </Tabs.Trigger>
-            ))}
-          </Tabs.List>
-          <div className={"flex justify-center"}>
-            {steadyParticipantsData.participants.map((participant) => (
-              <Tabs.Content
-                key={participant.id}
-                value={participant.id.toString()}
-                className="p-30"
-              >
-                {!participantDetailsData.isDeleted ? (
-                  <div>
-                    <div
-                      className={cn(
-                        "flex w-full flex-col justify-center text-center",
-                      )}
-                    >
-                      <div className={cn(subContentStyles, "items-center")}>
-                        <div className={cn(subMyPageTextStyles.title)}>
-                          멤버 정보
-                        </div>
-                        <Image
-                          src={participantDetailsData.user.profileImage}
-                          alt={"내 프로필 이미지"}
-                          width={150}
-                          height={150}
-                          className="border-black rounded-full border-2 transition-opacity group-hover:opacity-50"
-                        />
-                        <div
-                          className={cn(
-                            subMyPageTextStyles.content,
-                            "flex flex-col gap-y-5",
-                          )}
-                        >
-                          {participantDetailsData.user.nickname}
-                          <div className={cn("justify-center")}>
-                            <Badge size={"1"}>
-                              {participantDetailsData.user.position.name}
-                            </Badge>
-                          </div>
-                          <div className={cn("my-10 justify-center")}>
-                            {participantDetailsData.user.stacks.map((stack) => (
-                              <Badge
-                                className={cn("mx-5")}
-                                size={"1"}
-                                color={"gray"}
-                                key={stack.id}
-                              >
-                                {stack.name}
-                              </Badge>
-                            ))}
-                          </div>
-                          <div
-                            className={cn(
-                              "my-10 justify-center text-st-gray-250",
-                            )}
-                          >
-                            {participantDetailsData.user.bio ??
-                              "소개가 없습니다."}
-                          </div>
-                          <div
-                            className={cn(
-                              "flex items-center justify-center gap-30",
-                            )}
-                          >
-                            {participantDetailsData.userCards.map((card) => (
-                              <div
-                                key={card.cardId}
-                                className={cn(
-                                  subMyPageTextStyles.content,
-                                  "flex h-65 flex-col justify-between",
-                                )}
-                              >
-                                <Image
-                                  src={card.imageUrl}
-                                  alt="카드 이미지"
-                                  width={30}
-                                  height={30}
-                                />
-                                <div
-                                  className={cn(subMyPageTextStyles.content)}
-                                >
-                                  {`( ${card.count} )`}
-                                </div>
-                              </div>
-                            ))}
-                          </div>
-                          <div
-                            className={cn(subMyPageTextStyles.content, "my-20")}
-                          >
-                            <div
-                              className={cn("my-10 flex flex-col items-center")}
-                            >
-                              {participantDetailsData.reviews.length}개의 리뷰
-                            </div>
-                            <ScrollArea
-                              className={cn(
-                                "flex h-230 w-450 flex-col gap-20 border-1",
-                              )}
-                            >
-                              {participantDetailsData.reviews.map(
-                                (review, idx) => (
-                                  <>
-                                    <div
-                                      key={idx}
-                                      className="flex items-center justify-center text-st-gray-250"
-                                    >
-                                      {review}
-                                    </div>
-                                    <Separator className="h-1 w-auto bg-st-gray-100" />
-                                  </>
-                                ),
-                              )}
-                            </ScrollArea>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    {steadyDetailsData.leaderResponse.id !==
-                      participantDetailsData.user.userId && (
-                      <div className={cn(subContentStyles)}>
-                        <div className={cn(subBoxStyles, "justify-between")}>
-                          <div className={cn(subMyPageTextStyles.content)}>
-                            스테디 참여자 추방 시 되돌릴 수 없습니다.
-                          </div>
-                          {/* TODO: 유저 추방 기능 구현 */}
-                          <AlertModal
-                            trigger={
-                              <Button
-                                className={`${buttonSize.lg} bg-st-red text-st-white`}
-                              >
-                                멤버 추방
-                              </Button>
-                            }
-                            actionButton={
-                              <Button
-                                className={`${buttonSize.sm} bg-st-red text-st-white`}
-                                onClick={handleBanMember}
-                              >
-                                추방
-                              </Button>
-                            }
-                          >
-                            정말 해당 멤버를 추방하시겠습니까?
-                          </AlertModal>
-                        </div>
-                      </div>
+      <Separator className="h-5 w-full bg-st-gray-400" />
+
+      <Tabs.Root
+        defaultValue={`${steadyParticipantsData.participants[0].id}`}
+        className="flex flex-col overflow-x-hidden max-sm:w-400 sm:w-450 md:w-600 lg:w-800 xl:w-1000"
+      >
+        <Tabs.List className="flex w-full gap-10">
+          {steadyParticipantsData.participants.map((participant) => (
+            <Tabs.Trigger
+              key={participant.id}
+              value={participant.id.toString()}
+              className="cursor-pointer"
+              onClick={() => {
+                setCurrentUserIndex(
+                  steadyParticipantsData.participants.findIndex(
+                    (item) => item.id === participant.id,
+                  ),
+                );
+              }}
+            >
+              {participant.nickname}
+            </Tabs.Trigger>
+          ))}
+        </Tabs.List>
+        <div className={"flex justify-center"}>
+          {steadyParticipantsData.participants.map((participant) => (
+            <Tabs.Content
+              key={participant.id}
+              value={participant.id.toString()}
+              className="p-30"
+            >
+              {!participantDetailsData.isDeleted ? (
+                <div>
+                  <div
+                    className={cn(
+                      "flex w-full flex-col justify-center text-center",
                     )}
-                  </div>
-                ) : (
-                  <div>
-                    <div className="flex h-full w-full flex-col items-center justify-evenly gap-20">
+                  >
+                    <div className={cn(subContentStyles, "items-center")}>
+                      <div className={cn(subMyPageTextStyles.title)}>
+                        멤버 정보
+                      </div>
                       <Image
-                        src={DeleteUserLogo}
-                        alt="탈퇴한 유저 이미지"
+                        src={participantDetailsData.user.profileImage}
+                        alt={"내 프로필 이미지"}
                         width={150}
                         height={150}
+                        className="border-black rounded-full border-2 transition-opacity group-hover:opacity-50"
                       />
-                      <div className="text-25 font-bold">
-                        탈퇴한 유저 입니다.
+                      <div
+                        className={cn(
+                          subMyPageTextStyles.content,
+                          "flex flex-col gap-y-5",
+                        )}
+                      >
+                        {participantDetailsData.user.nickname}
+                        <div className={cn("justify-center")}>
+                          <Badge size={"1"}>
+                            {participantDetailsData.user.position.name}
+                          </Badge>
+                        </div>
+                        <div className={cn("my-10 justify-center")}>
+                          {participantDetailsData.user.stacks.map((stack) => (
+                            <Badge
+                              className={cn("mx-5")}
+                              size={"1"}
+                              color={"gray"}
+                              key={stack.id}
+                            >
+                              {stack.name}
+                            </Badge>
+                          ))}
+                        </div>
+                        <div
+                          className={cn(
+                            "my-10 justify-center text-st-gray-250",
+                          )}
+                        >
+                          {participantDetailsData.user.bio ??
+                            "소개가 없습니다."}
+                        </div>
+                        <div
+                          className={cn(
+                            "flex items-center justify-center gap-30",
+                          )}
+                        >
+                          {participantDetailsData.userCards.map((card) => (
+                            <div
+                              key={card.cardId}
+                              className={cn(
+                                subMyPageTextStyles.content,
+                                "flex h-65 flex-col justify-between",
+                              )}
+                            >
+                              <Image
+                                src={card.imageUrl}
+                                alt="카드 이미지"
+                                width={30}
+                                height={30}
+                              />
+                              <div className={cn(subMyPageTextStyles.content)}>
+                                {`( ${card.count} )`}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                        <div
+                          className={cn(subMyPageTextStyles.content, "my-20")}
+                        >
+                          <div
+                            className={cn("my-10 flex flex-col items-center")}
+                          >
+                            {participantDetailsData.reviews.length}개의 리뷰
+                          </div>
+                          <ScrollArea
+                            className={cn(
+                              "flex h-230 w-450 flex-col gap-20 border-1",
+                            )}
+                          >
+                            {participantDetailsData.reviews.map(
+                              (review, idx) => (
+                                <>
+                                  <div
+                                    key={idx}
+                                    className="flex items-center justify-center text-st-gray-250"
+                                  >
+                                    {review}
+                                  </div>
+                                  <Separator className="h-1 w-auto bg-st-gray-100" />
+                                </>
+                              ),
+                            )}
+                          </ScrollArea>
+                        </div>
                       </div>
                     </div>
-                    <div className={cn(subContentStyles, "pt-30")}>
+                  </div>
+                  {steadyDetailsData.leaderResponse.id !==
+                    participantDetailsData.user.userId && (
+                    <div className={cn(subContentStyles)}>
                       <div className={cn(subBoxStyles, "justify-between")}>
                         <div className={cn(subMyPageTextStyles.content)}>
                           스테디 참여자 추방 시 되돌릴 수 없습니다.
@@ -397,13 +355,53 @@ const SteadyManagePage = ({ params }: { params: { id: string } }) => {
                         </AlertModal>
                       </div>
                     </div>
+                  )}
+                </div>
+              ) : (
+                <div>
+                  <div className="flex h-full w-full flex-col items-center justify-evenly gap-20">
+                    <Image
+                      src={DeleteUserLogo}
+                      alt="탈퇴한 유저 이미지"
+                      width={150}
+                      height={150}
+                    />
+                    <div className="text-25 font-bold">탈퇴한 유저 입니다.</div>
                   </div>
-                )}
-              </Tabs.Content>
-            ))}
-          </div>
-        </Tabs.Root>
-      </div>
+                  <div className={cn(subContentStyles, "pt-30")}>
+                    <div className={cn(subBoxStyles, "justify-between")}>
+                      <div className={cn(subMyPageTextStyles.content)}>
+                        스테디 참여자 추방 시 되돌릴 수 없습니다.
+                      </div>
+                      {/* TODO: 유저 추방 기능 구현 */}
+                      <AlertModal
+                        trigger={
+                          <Button
+                            className={`${buttonSize.lg} bg-st-red text-st-white`}
+                          >
+                            멤버 추방
+                          </Button>
+                        }
+                        actionButton={
+                          <Button
+                            className={`${buttonSize.sm} bg-st-red text-st-white`}
+                            onClick={handleBanMember}
+                          >
+                            추방
+                          </Button>
+                        }
+                      >
+                        정말 해당 멤버를 추방하시겠습니까?
+                      </AlertModal>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </Tabs.Content>
+          ))}
+        </div>
+      </Tabs.Root>
+
       <Separator className="h-5 w-auto bg-st-gray-400" />
     </div>
   );
