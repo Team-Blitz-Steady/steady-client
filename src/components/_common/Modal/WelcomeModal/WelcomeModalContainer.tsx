aceae9d8d2279cb1b33296a0e4229e8716fe9eaa
JiYoon <angella990825@gmail.com>
2023-12-04 18:33:53 +0900

    💄 Style(#47): 모달 반응형 적용

--- src/components/_common/Modal/WelcomeModal/WelcomeModalContainer.tsx
+++ src/components/_common/Modal/WelcomeModal/WelcomeModalContainer.tsx
@@ -23,20 +23,24 @@ const WelcomeModalContainer = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-evenly">
       <div className="flex w-full flex-col items-center justify-center gap-10">
-        <div className="text-25 font-bold">🧐 스테디에 참여하고 싶은 사람</div>
+        <div className="text-20 font-bold sm:text-25">
+          🧐 스테디에 참여하고 싶은 사람
+        </div>
 
         <button
           onClick={handleClickSearchAction}
-          className="text-black h-200 w-full rounded-10 text-30 font-bold hover:bg-st-skyblue-50"
+          className="text-black h-200 w-full rounded-10 text-25 font-bold hover:bg-st-skyblue-50 sm:text-30"
         >
           🔎 스테디 검색
         </button>
       </div>
       <div className="flex w-full flex-col items-center justify-center gap-10">
-        <div className="text-25 font-bold">🤗 스테디를 운영하고 싶은 사람</div>
+        <div className="text-20 font-bold sm:text-25">
+          🤗 스테디를 운영하고 싶은 사람
+        </div>
 
         <button
-          className="text-black h-200 w-full rounded-10 text-30 font-bold hover:bg-st-skyblue-50"
+          className="text-black h-200 w-full rounded-10 text-25 font-bold hover:bg-st-skyblue-50 sm:text-30"
           onClick={handleClickCreateAction}
         >
           ✍️ 스테디 생성
