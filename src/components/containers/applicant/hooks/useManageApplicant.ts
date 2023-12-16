005afa1ba0745263593ea0bdebfb4c7df21d993b
JiYoon <angella990825@gmail.com>
2023-12-16 21:03:26 +0900

    📝 Chore(#337): interface 삭제

--- src/components/containers/applicant/hooks/useManageApplicant.ts
+++ src/components/containers/applicant/hooks/useManageApplicant.ts
@@ -3,15 +3,13 @@ import { useQueryClient } from "@tanstack/react-query";
 import changeApplicationStatus from "@/services/application/changeApplicationStatus";
 import { getApplicantListKey } from "@/constants/queryKeys";
 
-interface ManageApplicantProps {
-  applicationId: string;
-  steadyId: string;
-}
-
 export const useManageApplicant = ({
   applicationId,
   steadyId,
-}: ManageApplicantProps) => {
+}: {
+  applicationId: string;
+  steadyId: string;
+}) => {
   const router = useRouter();
   const queryClient = useQueryClient();
 
