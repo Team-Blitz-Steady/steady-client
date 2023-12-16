2ecc51b4541008af6428460093846fa25d3c36d2
JiYoon <angella990825@gmail.com>
2023-12-16 19:05:56 +0900

    🚨 Fix(#332): queryClient의 clear로 에러페이지 reset 오류 해결

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -3,12 +3,14 @@
 import { useRouter } from "next/navigation";
 import useAuthStore from "@/stores/isAuth";
 import useLoginStepsStore from "@/stores/loginSteps";
+import { useQueryClient } from "@tanstack/react-query";
 import { deleteCookie } from "cookies-next";
 
 const Logout = () => {
   const router = useRouter();
   const { setIsAuth } = useAuthStore();
   const { setSteps } = useLoginStepsStore();
+  const queryClient = useQueryClient();
   // useEffect(() => {
   //   axios.get("https://steady-client.vercel.app/api/logout").then(() => {
   //     deleteCookie("access_token", {
@@ -33,6 +35,7 @@ const Logout = () => {
   });
   setSteps(0);
   setIsAuth(false);
+  queryClient.clear();
   router.replace("/");
   return null;
 };
