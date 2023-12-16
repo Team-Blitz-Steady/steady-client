bd9b9303261135fbd6b9804c1722674973ee0158
JiYoon <angella990825@gmail.com>
2023-12-16 21:06:31 +0900

    🔨 Refactor(#337): 신청자 승락 거절 버튼 컴포넌트화

--- src/app/(steady)/steady/applicant/[steady_id]/[application_id]/page.tsx
+++ src/app/(steady)/steady/applicant/[steady_id]/[application_id]/page.tsx
@@ -3,9 +3,7 @@
 import { Question } from "@/components/application";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import getApplicationDetails from "@/services/application/getApplicationDetails";
-import Button, { buttonSize } from "@/components/_common/Button";
-import { AlertModal } from "@/components/_common/Modal";
-import { useManageApplicant } from "@/components/containers/applicant/hooks/useManageApplicant";
+import ApplicantButtons from "@/components/containers/applicant/ApplicantButtons";
 import { getApplicationDetailsKey } from "@/constants/queryKeys";
 
 const UserApplicantPage = ({
@@ -15,10 +13,7 @@ const UserApplicantPage = ({
 }) => {
   const applicationId = params.application_id;
   const steadyId = params.steady_id;
-  const { handleClickAccept, handleClickReject } = useManageApplicant({
-    applicationId,
-    steadyId,
-  });
+
   const { data: applicationDetailsData } = useSuspenseQuery({
     queryKey: getApplicationDetailsKey(applicationId),
     queryFn: () => getApplicationDetails(applicationId),
@@ -38,40 +33,10 @@ const UserApplicantPage = ({
         </Question>
       ))}
       <div className="mt-auto flex justify-end gap-20">
-        <AlertModal
-          trigger={
-            <Button className={`${buttonSize.sm} bg-st-red text-st-white`}>
-              거절
-            </Button>
-          }
-          actionButton={
-            <Button
-              className={`${buttonSize.sm} bg-st-red text-st-white`}
-              onClick={handleClickReject}
-            >
-              거절
-            </Button>
-          }
-        >
-          <div className="text-18 font-bold">거절 하시겠습니까?</div>
-        </AlertModal>
-        <AlertModal
-          trigger={
-            <Button className={`${buttonSize.sm} bg-st-green text-st-white`}>
-              승인
-            </Button>
-          }
-          actionButton={
-            <Button
-              className={`${buttonSize.sm} bg-st-green text-st-white`}
-              onClick={handleClickAccept}
-            >
-              승인
-            </Button>
-          }
-        >
-          <div className="text-18 font-bold">승인 하시겠습니까?</div>
-        </AlertModal>
+        <ApplicantButtons
+          applicationId={applicationId}
+          steadyId={steadyId}
+        />
       </div>
     </div>
   );
